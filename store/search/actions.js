606a16c8fb237dde937dfbc158d52fdfab984395
sagar <sagar.talla@fptechscience.com>
2018-04-13 10:22:58 +0530

    Fix Pagination

--- store/search/actions.js
+++ store/search/actions.js
@@ -9,7 +9,7 @@ const actions = {
 const actionCreaters = {
   getSearchResults: (params, loadMore) => (dispatch, getState) => {
     const state = getState();
-    const data = Object.assign({}, state.searchReducer.data.searchDetails, state.searchReducer.data.paginationDetails, params);
+    const data = Object.assign({}, state.searchReducer.data.searchDetails, state.searchReducer.data.paginationDetails, loadMore ? {} : { pageNum: 1 }, params);
     return dispatch({
       type: loadMore ? actions.GET_MORE_RESULTS : actions.GET_SEARCH_RESULTS,
       // type: actions.GET_SEARCH_RESULTS,
