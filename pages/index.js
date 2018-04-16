97685b46b52e5cbfd25f82eef9aaf20db233e51a
sagar <sagar.talla@fptechscience.com>
2018-04-16 17:42:12 +0530

    Added category filters

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,26 @@ import makeStore from '../store';
 import { actionCreaters } from '../store/search';
 import Layout from '../layout/main';
 import Search from '../components/Search';
+import { selectors } from '../store/search/index';
 
 class Page extends Component {
   static async getInitialProps({ store, isServer, query }) {
-    console.log(query);
+    const categoryFilter = {};
+    if(query.category) {
+      /* redundant call */
+      await store.dispatch(actionCreaters.getSearchResults({
+        categoryFilter,
+        country: 'UAE',
+        pageSize: 100,
+        query: 'mobile',
+        language: 'en',
+        customFilters: {},
+        pageNum: 1,
+      }));
+      categoryFilter.id = selectors.getCategoryId(store, query);
+    }
     await store.dispatch(actionCreaters.getSearchResults({
-      categoryFilter: {},
+      categoryFilter,
       country: 'UAE',
       pageSize: 100,
       query: 'mobile',
