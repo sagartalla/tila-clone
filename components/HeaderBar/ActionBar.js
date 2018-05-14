b217c5925fe7c92aafcc1be1232b43b820ae278a
Mohan Natraj Palanivel <mohan.palanivel@fptechscience.com>
2018-05-14 17:08:17 +0530

    indentation

--- components/HeaderBar/ActionBar.js
+++ components/HeaderBar/ActionBar.js
@@ -1,47 +1,51 @@
-import { connect } from 'react-redux';
-import { Modal } from 'react-router-modal';
-import styles from './header.styl';
-import { getCookie } from '../../components/Overlay/Login/CookieMethods';
-import Login from '../Login';
+import { connect } from "react-redux";
+import { Modal } from "react-router-modal";
+import styles from "./header.styl";
+import { getCookie } from "../../components/Overlay/Login/CookieMethods";
+import Login from "../Login";
 
 export default class ActionBar extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            showLogin: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLogin: false
+    };
+  }
 
-    toggleLoginModal = (e, toggle) => {
-        this.setState({
-            showLogin: toggle
-        })
-    }
+  toggleLoginModal = (e, toggle) => {
+    this.setState({
+      showLogin: toggle
+    });
+  };
 
-    postLogin = () => {
-        this.toggleLoginModal(null, false);
-    }
+  postLogin = () => {
+    this.toggleLoginModal(null, false);
+  };
 
-    render() {
-        return (
-            <div className={styles['actionbar-wrapper']}>
-                {getCookie('access_token') ? 
-                    <div className={styles['action-item']}>Logged In</div>
-                :
-                    <div className={styles['action-item']} onClick={(e) => this.toggleLoginModal(e, true)}>Log In</div>
-                }
-                <div className={styles['action-item']}></div>
-                <div className={styles['action-item']}></div>
-                <div className={styles['action-item']}></div>
-                {this.state.showLogin ?
-                <Modal onBackdropClick={(e) => this.toggleLoginModal(e,false)}>
-                    <Login postLogin={this.postLogin}/>
-                </Modal>
-                : ''
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={styles["actionbar-wrapper"]}>
+        {getCookie("access_token") ? (
+          <div className={styles["action-item"]}>Logged In</div>
+        ) : (
+          <div
+            className={styles["action-item"]}
+            onClick={e => this.toggleLoginModal(e, true)}
+          >
+            Log In
+          </div>
+        )}
+        <div className={styles["action-item"]} />
+        <div className={styles["action-item"]} />
+        <div className={styles["action-item"]} />
+        {this.state.showLogin ? (
+          <Modal onBackdropClick={e => this.toggleLoginModal(e, false)}>
+            <Login postLogin={this.postLogin} />
+          </Modal>
+        ) : (
+          null
+        )}
+      </div>
+    );
+  }
 }
-
