d0d2a4b4c4b50c51a42b0c4708db0a5da3c10af5
sagar <sagar.talla@fptechscience.com>
2018-05-21 15:43:32 +0530

    Add default route

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const routes = require('next-routes')();
 
 routes
+  .add({ name: 'CAM', pattern: '/cam', page: 'cam' })
   .add({ name: 'ORDER', pattern: '/cam/orders/:orderId', page: 'order' })
-  .add({ name: 'CAM', pattern: '/cam/:tabDetails+', page: 'cam' })
+  .add({ name: 'CAM tabs', pattern: '/cam/:tabDetails+', page: 'cam' })
   .add({ name: 'product', pattern: '/product', page: 'product' })
   .add({ name: 'search', pattern: '/:category?/:subCategory?', page: 'index' });
   
