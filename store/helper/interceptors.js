3d94685e50fac5af8de5aa6ae2a576e5e6d438d4
sagar <sagar.talla@fptechscience.com>
2018-05-25 21:40:32 +0530

    Add sliding window

--- store/helper/interceptors.js
+++ store/helper/interceptors.js
@@ -1,110 +1,130 @@
 import shajs from 'sha.js';
 import { init as initApm } from 'elastic-apm-js-base';
 import fp, * as _ from 'lodash/fp';
+import axios from 'axios';
 
-import { searchServiceInstance, listingServiceInstance, addressServiceInstance, pimServiceInstance, catalogServiceInstance, authServiceInstance, paymentInstance, transacationRedirectUrlInstance, orderServiceInstance } from './services';
+import { searchServiceInstance, listingServiceInstance, addressServiceInstance, pimServiceInstance, catalogServiceInstance, authServiceInstance, paymentInstance, transacationRedirectUrlInstance, orderServiceInstance, cartServiceInstance } from './services';
 
 import apmConfig from '../../apm.config';
+import constants from './constants';
 
 const apm = initApm({
-    serviceName: apmConfig.serviceName,
-    serverUrl: apmConfig.serverUrl,
+  serviceName: apmConfig.serviceName,
+  serverUrl: apmConfig.serverUrl,
 });
 
 export const authToken = () => {
-    try {
-        if (localStorage) {
-            const auth = localStorage.auth
-            return JSON.parse(auth).access_token;
-        } else {
-            return false;
-        }
-    } catch (e) {
-        console.log(e);
+  try {
+    if (localStorage) {
+      const auth = localStorage.auth
+      return JSON.parse(auth).access_token;
+    } else {
+      return false;
     }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 
-const reqInterceptor = (serviceName) => (config) => {
-    config.transaction = apm.startTransaction(`${serviceName} Service`, 'custom')
-    config.httpSpan = config.transaction ? config.transaction.startSpan(`FETCH ${JSON.stringify(config)}`, 'http') : null;
-    return config;
+const apmReqInterceptor = (serviceName) => (config) => {
+  config.transaction = apm.startTransaction(`${serviceName} Service`, 'custom')
+  config.httpSpan = config.transaction ? config.transaction.startSpan(`FETCH ${JSON.stringify(config)}`, 'http') : null;
+  return config;
 }
 
-const resInterceptor = (serviceName) => (response) => {
-    const { httpSpan, transaction } = response.config;
-    httpSpan && httpSpan.end();
-    transaction && transaction.end();
-    return response;
+const apmResInterceptor = (serviceName) => (response) => {
+  const { httpSpan, transaction } = response.config;
+  httpSpan && httpSpan.end();
+  transaction && transaction.end();
+  return response;
 }
 
-searchServiceInstance.interceptors.request.use(_.compose(reqInterceptor('SEARCH')));
-searchServiceInstance.interceptors.response.use(_.compose(resInterceptor('SEARCH')));
+const errorInterceptor = (err) => {
+  try {
+    if (err.response.status) {
+      const auth = JSON.parse(localStorage.getItem('auth'));
+      axios.post(`${constants.AUTH_API_URL}/api/v1/refresh`, {
+        'auth_version': 'V1',
+        'refresh_token': auth.refresh_token
+      }).then((res) => {
+        auth.access_token = res.data.access_token
+        localStorage.setItem('auth', JSON.stringify(auth));
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  return err;
+}
 
-listingServiceInstance.interceptors.request.use(_.compose(reqInterceptor('LISTING')));
-listingServiceInstance.interceptors.response.use(_.compose(resInterceptor('LISTING')));
+searchServiceInstance.interceptors.request.use(_.compose(apmReqInterceptor('SEARCH')));
+searchServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('SEARCH')), _.compose(errorInterceptor));
 
-catalogServiceInstance.interceptors.request.use(_.compose(reqInterceptor('CATALOG')));
-catalogServiceInstance.interceptors.response.use(_.compose(resInterceptor('CATALOG')));
+listingServiceInstance.interceptors.request.use(_.compose(apmReqInterceptor('LISTING')));
+listingServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('LISTING')), _.compose(errorInterceptor));
 
-authServiceInstance.interceptors.request.use(_.compose(reqInterceptor('AUTH')));
-authServiceInstance.interceptors.response.use(_.compose(resInterceptor('AUTH')));
+catalogServiceInstance.interceptors.request.use(_.compose(apmReqInterceptor('CATALOG')));
+catalogServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('CATALOG')), _.compose(errorInterceptor));
 
-transacationRedirectUrlInstance.interceptors.request.use(_.compose(reqInterceptor('AUTH')));
-transacationRedirectUrlInstance.interceptors.response.use(_.compose(resInterceptor('AUTH')));
+authServiceInstance.interceptors.request.use(_.compose(apmReqInterceptor('AUTH')));
+authServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('AUTH')), _.compose(errorInterceptor));
+
+transacationRedirectUrlInstance.interceptors.request.use(_.compose(apmReqInterceptor('AUTH')));
+transacationRedirectUrlInstance.interceptors.response.use(_.compose(apmResInterceptor('AUTH')), _.compose(errorInterceptor));
 
 pimServiceInstance.interceptors.request.use(_.compose(
-    reqInterceptor('PIM'),
-    (config) => {
-        const tenantId = '5ab0f832a206e8419416f74f';
-        const key = 'lcjkxcjzlxcko45';
-        const requestId = (`${Math.random()} `).substring(2, 10) + (`${Math.random()} `).substring(2, 10);
-        const copyConfig = Object.assign({}, config);
-        const stringifiedData = JSON.stringify(config.data) || '';
-        copyConfig.headers = {
-            tenantId,
-            'Request-Id': requestId,
-            Hash: shajs('sha256').update(`${key}${stringifiedData}${requestId}`).digest('hex'),
-        };
-        return copyConfig;
-    }
+  apmReqInterceptor('PIM'),
+  (config) => {
+    const tenantId = '5ab0f832a206e8419416f74f';
+    const key = 'lcjkxcjzlxcko45';
+    const requestId = (`${Math.random()} `).substring(2, 10) + (`${Math.random()} `).substring(2, 10);
+    const copyConfig = Object.assign({}, config);
+    const stringifiedData = JSON.stringify(config.data) || '';
+    copyConfig.headers = {
+      tenantId,
+      'Request-Id': requestId,
+      Hash: shajs('sha256').update(`${key}${stringifiedData}${requestId}`).digest('hex'),
+    };
+    return copyConfig;
+  }
 ));
-pimServiceInstance.interceptors.response.use(_.compose(resInterceptor('PIM')));
+pimServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('PIM'))), _.compose(errorInterceptor);
 
 addressServiceInstance.interceptors.request.use(_.compose(
-    reqInterceptor('ADDRESS'),
-    (config) => {
-        config.headers = { "x-access-token": authToken(), "x-country-code": "SAE" };
-        return config;
-    }
+  apmReqInterceptor('ADDRESS'),
+  (config) => {
+    config.headers = { "x-access-token": authToken(), "x-country-code": "SAE" };
+    return config;
+  }
 ));
-addressServiceInstance.interceptors.response.use(_.compose(resInterceptor('ADDRESS')))
+addressServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('ADDRESS')), _.compose(errorInterceptor))
 
 orderServiceInstance.interceptors.request.use(_.compose(
-    reqInterceptor('ORDER'),
-    (config) => {
-        config.headers = { "x-access-token": authToken(), "x-country-code": "SAE" };
-        return config;
-    }
+  apmReqInterceptor('ORDER'),
+  (config) => {
+    config.headers = { "x-access-token": authToken(), "x-country-code": "SAE" };
+    return config;
+  }
 ));
-orderServiceInstance.interceptors.response.use(_.compose(resInterceptor('ORDER')))
+orderServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('ORDER')), _.compose(errorInterceptor))
 
 paymentInstance.interceptors.request.use(_.compose(
-    reqInterceptor('PAYMENT'),
-    (config) => {
-        config.headers = { "x-access-token": authToken(), "x-country-code": "SAE" };
-        return config;
-    }
+  apmReqInterceptor('PAYMENT'),
+  (config) => {
+    config.headers = { "x-access-token": authToken(), "x-country-code": "SAE" };
+    return config;
+  }
 ));
-paymentInstance.interceptors.response.use(_.compose(resInterceptor('PAYMENT')))
+paymentInstance.interceptors.response.use(_.compose(apmResInterceptor('PAYMENT')), _.compose(errorInterceptor))
 
 cartServiceInstance.interceptors.request.use(_.compose(
-    reqInterceptor('CART'),
-    (config) => {
-        config.headers = { "x-country-code": "ksa", "x-session-id": "asdasd", "x-access-token": authToken(), "x-language": 'en' };
-        return config;
-    }
+  apmReqInterceptor('CART'),
+  (config) => {
+    config.headers = { "x-country-code": "ksa", "x-session-id": "asdasd", "x-access-token": authToken(), "x-language": 'en' };
+    return config;
+  }
 ));
-cartServiceInstance.interceptors.response.use(_.compose(resInterceptor('PAYMENT')))
+cartServiceInstance.interceptors.response.use(_.compose(apmResInterceptor('CART')), _.compose(errorInterceptor))
 
 export default {};
