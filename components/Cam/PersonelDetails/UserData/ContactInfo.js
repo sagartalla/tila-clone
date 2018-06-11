063e536d95c95b9f9d291ef67d7d9b0590bdf3e3
Parul Priyanka <parulpriyanka@FPTECHS060s-MacBook-Pro.local>
2018-06-11 15:01:49 +0530

    Adding email verification icon

--- components/Cam/PersonelDetails/UserData/ContactInfo.js
+++ components/Cam/PersonelDetails/UserData/ContactInfo.js
@@ -29,7 +29,7 @@ class ContactInfo extends React.Component {
   }
 
   render() {
-    const { mailId, email, mobile_no, lastUpdated, phoneNum } = this.props.contactInfo ? this.props.contactInfo : { email: "", mobile_no: "", lastUpdated: "not available" };
+    const { mailId, email, mobile_no, lastUpdated, phoneNum, email_verified } = this.props.contactInfo ? this.props.contactInfo : { email: "", mobile_no: "", lastUpdated: "not available" };
     const { element, show, contactInfo } = this.state;
     return (
       <div className={`${commonStyle['ml-15']} ${commonStyle['mt-1o']} ${commonStyle['mb-10']}`}>
@@ -43,10 +43,14 @@ class ContactInfo extends React.Component {
             <span>Email</span>
           </Col>
           <Col xs={12} md={8} className={`${commonStyle['p-0']} ${commonStyle['m-5']}`}>
-            <Col xs={6} md={9}>
+            <Col xs={6} md={8}>
               <span>{mailId}</span>
             </Col>
-            <Col xs={6} md={3}>
+            <Col xs={3} md={2}>
+              {/* Add Not verified Image here*/}
+              <a><span className={email_verified == 'NV' ? `${commonStyle['showDiv']}` : `${commonStyle['hideDiv']}`}>!!</span></a>
+            </Col>
+            <Col xs={3} md={2}>
               {/* <span className={`${commonStyle['float-r']} ${commonStyle['p-0']} ${commonStyle['ml-5']}`}>
                 <a onClick={this.handleShow(true, `email`)}>Edit</a>
               </span> */}
