f0b30f2d57bd7f6894fd2c9ca5ba5e29818b1113
Parul Priyanka <parulpriyanka@FPTECHS060s-MacBook-Pro.local>
2018-06-14 16:10:59 +0530

    Order list on thankyou page

--- components/Thankyou/index.js
+++ components/Thankyou/index.js
@@ -8,7 +8,7 @@ import { actionCreators, selectors } from '../../store/thankyou';
 
 import PaymentStatus from './includes/PaymentStatus';
 import Summary from './includes/Summary';
-
+import OrderList from './includes/OrderList';
 
 import styles from './thankyou.styl';
 
@@ -22,20 +22,26 @@ class Thankyou extends Component {
   }
 
   componentDidMount() {
-    const { orderId, status } = this.props;
+    const { orderId, status, getOrderStatusDetails } = this.props;
     this.setState({
       orderState: status,
       orderId: orderId
     });
     if (status == "SUCCESSFUL") {
-      this.props.getOrderStatusDetails(orderId);
+      getOrderStatusDetails(orderId);
     }
   }
 
   render() {
     const { orderDetails } = this.props;
     const { orderState, orderId } = this.state;
-    const summary = orderState == "SUCCESSFUL" ? (<Summary orderId={orderId} orderDetails={orderDetails} />) : null;
+    const summary = orderState == "SUCCESSFUL" ?
+      (
+        <div>
+          <Summary orderId={orderId} orderDetails={orderDetails} />
+          <OrderList orderDetails={orderDetails} />
+        </div>
+      ) : null;
     return (
       <div className={styles['thankyou']}>
         <Grid>
@@ -46,6 +52,7 @@ class Thankyou extends Component {
     )
   }
 }
+
 
 const mapStateToProps = (store) => ({
   orderDetails: selectors.getFinalOrderDetails(store)
