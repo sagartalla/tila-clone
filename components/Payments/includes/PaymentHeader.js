31721d15e199941bb1c094ed286414b42a39ac53
ravikumar <ravikiran.bandla@fptechscience.com>
2018-06-20 13:06:10 +0530

    header logo vhange

--- components/Payments/includes/PaymentHeader.js
+++ components/Payments/includes/PaymentHeader.js
@@ -1,13 +1,16 @@
 import { Grid, Row, Col } from 'react-bootstrap';
+import SVGCompoent from '../../common/SVGComponet';
 import { mergeCss } from '../../../utils/cssUtil';
 const styles = mergeCss('components/Payments/payment');
 
 const PaymentHeader = props => (
-  <div className={`${styles['payment-header']} ${styles['p-10']} `}>
+  <div className={`${styles['payment-header']} ${styles['p-20']} ${styles['bg-white']} ${styles['default-shadow']}`}>
     <Grid>
       <Row>
-        <Col md={12}>
-          LOGO
+        <Col md={12} className={`${styles['pl-0']}`}>
+          <div className={`${styles['flex-center']} ${styles['logos-part']}`}>
+            <SVGCompoent clsName={`${styles['default-logo']}`} src="icons/logos/logo" />
+          </div>
         </Col>
       </Row>
     </Grid>
