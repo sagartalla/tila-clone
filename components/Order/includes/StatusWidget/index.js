dfa69f9af90c7390f601b5f9f0ed3fb79ceea6f8
Mehraj Reniman <mehrajreniman@mehraj.local>
2018-06-28 17:20:18 +0530

    order widget changes.

--- components/Order/includes/StatusWidget/index.js
+++ components/Order/includes/StatusWidget/index.js
@@ -5,23 +5,49 @@ import { Row, Col } from 'react-bootstrap';
 import { mergeCss } from '../../../../utils/cssUtil';
 const styles = mergeCss('components/Order/includes/StatusWidget/statusWidget');
 
-const statuses = ['APPROVED', 'PACKED', 'SHIPPED', 'DELIVERED'];
-
 const StatusWidget = ({ currentStatus }) => {
-  const currentStausIndex = statuses.indexOf(currentStatus);
+
+  const len = Object.keys(currentStatus[0].state_times).length;
+  const pivot = (100 / (len - 1));
+  let barLen = 0;
+
   return (
-    <div className={`${styles['widget-cont']} ${styles['pt-5']}`}>
+    <div className={`${styles['status-widget']} ${styles['pt-5']} ${styles['relative']}`}>
       <Row className={styles['m-0']}>
-        {statuses.map((status, index) => (
-          <Col key={status} md={3} className={`${styles['item-cont']} ${(currentStausIndex >= index) ? (currentStausIndex === index) ? styles['current'] : styles['done'] : ''}`}>
-            <div className={`${styles['pt-10']} ${styles['item']}` }>
-              <span>{status}</span>
-            </div>
-          </Col>
-        ))}
+        <div className={`${styles['gray-line']}`}>
+          {
+            Object.keys(currentStatus[0].state_times).map((i, k, index) => {
+              const { completed } = currentStatus[0].state_times[i];
+              if (completed) {
+                barLen = pivot * k;
+              }
+              return (
+                <div className={`${styles['point']} ${completed ? styles['active'] : ''} `} style={{ left: `${k == 0 ? '0' : (pivot * k)}%` }}>
+                  <span>{i}</span>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div style={{ width: `${barLen}%` }} className={`${styles['green-line']} ${styles['relative']}`}></div>
       </Row>
-    </div>
-  );
+    </div >
+  )
+
+
+  // return (
+  //   <div className={`${styles['widget-cont']} ${styles['pt-5']}`}>
+  //     <Row className={styles['m-0']}>
+  //       {statuses.map((status, index) => (
+  //         <Col key={status} md={3} className={`${styles['item-cont']} ${(currentStausIndex >= index) ? (currentStausIndex === index) ? styles['current'] : styles['done'] : ''}`}>
+  //           <div className={`${styles['pt-10']} ${styles['item']}`}>
+  //             <span>{status}</span>
+  //           </div>
+  //         </Col>
+  //       ))}
+  //     </Row>
+  //   </div>
+  // );
 }
 
 export default StatusWidget;
