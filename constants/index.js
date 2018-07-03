d88965584e09c1e8210c6afe88389454350b84a9
sagar <sagar.talla@fptechscience.com>
2018-07-03 12:50:14 +0530

    Add Links

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,25 @@
-const mediaDomain = 'https://dev-catalog-imgs.s3.ap-south-1.amazonaws.com';
+import getConfig from 'next/config'
+const config = getConfig()
 
-export default { mediaDomain };
+export default {
+  local: {
+    mediaDomain: 'https://dev-catalog-imgs.s3.ap-south-1.amazonaws.com',
+    sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
+    customerHelp: 'https://fptsuae--tst1.custhelp.com/'
+  },
+  dev: {
+    mediaDomain: 'https://dev-catalog-imgs.s3.ap-south-1.amazonaws.com',
+    sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
+    customerHelp: 'https://fptsuae--tst1.custhelp.com/'
+  },
+  staging: {
+    mediaDomain: 'https://dev-catalog-imgs.s3.ap-south-1.amazonaws.com',
+    sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
+    customerHelp: 'https://fptsuae--tst1.custhelp.com/'
+  },
+  production: {
+    mediaDomain: 'https://dev-catalog-imgs.s3.ap-south-1.amazonaws.com',
+    sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
+    customerHelp: 'https://fptsuae--tst1.custhelp.com/'
+  }
+}[config.publicRuntimeConfig.env || 'dev'];
