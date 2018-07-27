42d275a296573606040ca69d23c0ea9572b53b86
sagar <sagar.talla@fptechscience.com>
2018-07-27 15:25:37 +0530

    Add Landing pages

---
+++ components/Landing/includes/Lifestyle/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Lifestyle = () => (
+  <div>Lifestyle</div>
+);
+
+export default Lifestyle;
---
+++ components/Landing/index.js
@@ -0,0 +1,28 @@
+import React, { Component,Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import HeaderBar from '../HeaderBar';
+import FooterBar from '../Footer';
+import Fashion from './includes/Fashion';
+import Electronics from './includes/Electronics';
+import Lifestyle from './includes/Lifestyle';
+
+const Landing = ({query}) => {
+  const { category,  } = query;
+  return (
+    <Fragment>
+      <HeaderBar />
+      {
+        {
+          'fashion': (<Fashion />),
+          'electronics': (<Electronics />),
+          'lifestyle': (<Lifestyle />),
+        }[category]
+      }
+      <FooterBar />
+    </Fragment>
+  )
+}
+
+
+export default Landing;
