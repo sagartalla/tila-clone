93d8b8f656f0fc01c6eab4a8627b9c6276b02a75
ravikumar <ravikiran.bandla@fptechscience.com>
2018-07-31 18:41:26 +0530

    payment methods static HTML content added. and vault undefined svg issue fixed.

--- components/Payments/includes/paymentpages/CashOnDelivery.js
+++ components/Payments/includes/paymentpages/CashOnDelivery.js
@@ -1,25 +1,55 @@
 import PropTypes from 'prop-types';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { languageDefinations } from '../../../../utils/lang/';
+import SVGComponent from '../../../common/SVGComponet';
 import { mergeCss } from '../../../../utils/cssUtil';
 const styles = mergeCss('components/Payments/payment');
 
 const CashOnDelivery = props => {
   const { PAYMENT_PAGE } = languageDefinations();
   return (
-    <div className={`${styles['reward-points']} ${styles['p-10']} `}>
-      <Grid>
-        <Row>
-          <Col md={12}>
-            Cash On Delivery
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} sm={12} xs={12}>
-            <button className={`${styles['fp-btn']} ${styles['fp-btn-primary']}`} onClick={props.makePayment}>Pay {props.orderRes.data.amount} {props.orderRes.data.currency}</button>
-          </Col>
-        </Row>
-      </Grid>
+    <div className={`${styles['cash-on-dly-points']}`}>
+      <Row className={styles['pl-40']}>
+        <Col md={12}>
+          <h4 className={`${styles['fontW300']} ${styles['fs-20']} ${styles['lgt-blue']} ${styles['mt-0']} ${styles['pb-10']}`}>Pay on Delivery</h4>
+          <div className={styles['checkbox-material']}>
+            <input id="pay-delivery" type="checkbox" />
+            <label for="pay-delivery"> I agree to pay cash on delivery </label>
+          </div>
+          <div className={`${styles['flx-spacebw-alignc']} ${styles['pt-30']} ${styles['pb-30']}`}>
+            <div>
+              <ul className={`${styles['pl-0']} ${styles['m-0']} ${styles['cash-tab']}`}>
+                <li>Visual</li>
+              </ul>
+              <div className={`${styles['captch-inn']} ${styles['p-20']}`}>
+                <span className={`${styles['flx-spacebw-alignc']} ${styles['refresh-part']} ${styles['pb-20']}`}>
+                  <span className={styles['fs-12']}>Drag and drop the Car into the box</span>
+                  <span className={`${styles['flex']} ${styles['refresh-part-inn']} ${styles['p-5']}`}><SVGComponent clsName={`${styles['refresh-icon']}`} src="icons/captcha-icons-list/refresh-icon" /></span>
+                </span>
+                <div>
+                  <div className={`${styles['flex-center']} ${styles['captcha-icon-part']}`}>
+                    <span className={styles['flex']}><SVGComponent clsName={`${styles['captcha-icon']}`} src="icons/captcha-icons-list/cat-icon" /></span>
+                    <span className={styles['flex']}><SVGComponent clsName={`${styles['captcha-icon']}`} src="icons/captcha-icons-list/ball-icon" /></span>
+                    <span className={styles['flex']}><SVGComponent clsName={`${styles['captcha-icon']}`} src="icons/captcha-icons-list/flower-icon" /></span>
+                    <span className={styles['flex']}><SVGComponent clsName={`${styles['car-icon']}`} src="icons/captcha-icons-list/car-icon" /></span>
+                    <span className={styles['flex']}><SVGComponent clsName={`${styles['captcha-icon']}`} src="icons/captcha-icons-list/chair-icon" /></span>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className={`${styles['flex']} ${styles['justify-center']}`}>
+              <SVGComponent clsName={`${styles['drop-box-icon']}`} src="icons/captcha-icons-list/box-icon" />
+              </div>
+              <span className={`${styles['fs-12']} ${styles['pt-20']}`}>Drop the Car icon into the box Above</span>
+            </div>
+          </div>
+        </Col>
+        <Col md={6} sm={12} xs={12}>
+          <button className={`${styles['fp-btn']} ${styles['fs-16']} ${styles['fontW600']} ${styles['fp-btn-primary']} ${styles['fp-btn-large']}`} onClick={props.makePayment}>Pay {props.orderRes.data.amount} {props.orderRes.data.currency}</button>
+        </Col>
+      </Row>
     </div>
   );
 }
