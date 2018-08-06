8ee2d2a531a35d040b8c61133a008cf55bb9c40d
Mehraj Reniman <mehrajreniman@mehraj.local>
2018-08-06 12:06:39 +0530

    save card

--- components/Payments/includes/paymentpages/PayOnline.js
+++ components/Payments/includes/paymentpages/PayOnline.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Row, Col } from 'react-bootstrap';
 
@@ -7,25 +8,48 @@ import { mergeCss } from '../../../../utils/cssUtil';
 const styles = mergeCss('components/Payments/payment');
 const { PAYMENT_PAGE } = languageDefinations();
 
-const PayOnline = props => (
-  <div className={`${styles['pay-online']} ${styles['p-10']} `}>
-    <Grid>
-      {/* <Row>
-          <Col md={12}>
-            {PAYMENT_PAGE.PAY_ONLINE}
-          </Col>
-        </Row> */}
-      <Row>
-        <Col md={12} sm={12} xs={12}>
-          {/* <button className={`${styles['fp-btn']} ${styles['fp-btn-primary']}`} onClick={props.makePayment}>Pay {props.orderRes.data.amount} {props.orderRes.data.currency}</button> */}
-          <iframe src={props.orderRes.payData.iframe_url} style={{ height: '406px', width: '600px', border: '0' }}>
+class PayOnline extends Component {
 
-          </iframe>
-        </Col>
-      </Row>
-    </Grid>
-  </div>
-);
+  constructor(props) {
+    super(props);
+    this.state = {
+      disableSaveCard: false
+    }
+
+    this.saveCardHandler = this.saveCardHandler.bind(this);
+  }
+
+  saveCardHandler(e) {
+    this.props.saveCard(e);
+    this.setState({ disableSaveCard: true })
+  }
+
+  render() {
+    const { orderRes } = this.props;
+    const { disableSaveCard } = this.state
+
+    return (
+      <div className={`${styles['pay-online']} ${styles['p-10']} `}>
+        <Grid>
+          <Row>
+            <Col md={12} sm={12} xs={12}>
+              {/* <button className={`${styles['fp-btn']} ${styles['fp-btn-primary']}`} onClick={props.makePayment}>Pay {props.orderRes.data.amount} {props.orderRes.data.currency}</button> */}
+              <iframe src={orderRes.payData.iframe_url} style={{ height: '406px', width: '600px', border: '0' }}></iframe>
+            </Col>
+            <Col md={12}>
+              <div className={styles['checkbox-material']}>
+                <input id="save-card" type="checkbox" onClick={this.saveCardHandler} disabled={disableSaveCard} />
+                <label for="save-card"> Save this card </label>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+
 
 PayOnline.propTypes = {
   makePayment: PropTypes.func.isRequired,
