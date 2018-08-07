96dd28140d221e6cff1520318477fe0948949751
ravikumar <ravikiran.bandla@fptechscience.com>
2018-08-07 12:07:19 +0530

    recent view issue done

--- components/Product/includes/RecentView.js
+++ components/Product/includes/RecentView.js
@@ -11,13 +11,13 @@ const RecentView = props => {
   return (
     <div className={`${styles['recentview-main']} ${styles['pt-25']} ${styles['pb-25']}`}>
       <h6 className={`${styles['recent-title']} ${styles['pt-15']} ${styles['pb-15']} ${styles['pl-15']} ${styles['fontW600']}`}><span className={styles['pl-15']}>RECENTLY VIEWED</span></h6>
-      <div>
+      <div className={styles['flex']}>
         {
           recentlyViewed.map((item) => {
             return (
-              <Col md={2} className={`${styles['pl-0']} ${styles['mr-30']}`}>
+              <Col md={2} className={`${styles['pl-0']}`}>
                 <a href={item.uri}>
-                  <div>
+                  <div className={`${styles['recentview-main-inn']} ${styles['flex']} ${styles['flex-colum']}`}>
                     <img src={"https://dev-catalog-imgs.s3.ap-south-1.amazonaws.com/" + item.im} className="img-responsive" />
                     <div>
                       <h6 className={`${styles['fs-10']} ${styles['mb-0']} ${styles['thick-gry-clr']} ${styles['elipsis']}`}>{item.nm}</h6>
