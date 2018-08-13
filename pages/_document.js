f118f10b91dc572db473aebdf327d56b80d89af2
sagar <sagar.talla@fptechscience.com>
2018-08-13 11:21:10 +0530

    Add language to document

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,19 @@ import Meta from '../components/Meta';
 
 export default class MyDocument extends Document {
   render() {
+    const props = this.props;
     return (
       <html>
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css" />
+          {
+            props.__NEXT_DATA__.query.language === 'ar'
+              ?
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css" />
+              :
+              null
+          }
+
           <Meta />
         </Head>
         <body>
