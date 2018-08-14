a379d1bc50107a234a5d6ca37f1c36c61c27c6df
ravikumar <ravikiran.bandla@fptechscience.com>
2018-08-14 19:07:21 +0530

    wish list pop added

--- components/Cam/Wishlist/includes/CartBottomPopup.js
+++ components/Cam/Wishlist/includes/CartBottomPopup.js
@@ -17,7 +17,7 @@ class CartBottomPopup extends Component {
     const { data, addToCart } = this.props;
     const settings = {
       arrows: true,
-      dots: true,
+      dots: false,
       infinite: false,
       speed: 500,
       slidesToShow: 7,
@@ -26,18 +26,21 @@ class CartBottomPopup extends Component {
     return (
       <div className={`${styles['cart-wishlist-popup']} ${styles['mr-15']}`}>
         <Grid>
-          <Slider {...settings}>
+          <h4 className={`${styles['cart-wishlist-popup-title']} ${styles['fs-30']} ${styles['flex-center']} ${styles['justify-center']} ${styles['lgt-blue']} ${styles['mt-5']} ${styles['mb-15']} ${styles['fontW300']}`}><span className={`${styles['bg-white']} ${styles['title-label']}`}>From Your Wishlist </span><span className={styles['title-border']}></span></h4>
+          <Slider {...settings} className={styles['cart-wishlist-popup-inn']}>
             {
               data.length > 0 && data.map((item, i) => {
                 const { wishlist_id, listing_id, brand_name, name, img, price, cur } = item;
                 return (
-                  <div key={i} className={`${styles['item']}`}>
-                    <img className={styles['img']} src={img} />
-                    <h5 className={`${styles['lgt-gry-clr']} ${styles['light-gry-clr']}`}>{name}</h5>
-                    <h4 className={`${styles['fontW600']} ${styles['light-gry-clr']} ${styles['mt-25']} ${styles['t-c']}`}>
-                      <span className={styles['fs-20']}>{price}</span> <span clsName={styles['fs-14']}> {cur}</span>
-                    </h4>
-                    <button id={listing_id} data-wish-id={wishlist_id} data-cart-res={true} className={`${styles['fp-btn']} ${styles['fp-btn-primary']}`} onClick={addToCart}>{WISH_LIST_PAGE.ADD_TO_CART_BTN}</button>
+                  <div key={i} className={`${styles['item']} ${styles['flex']} ${styles['flex-colum']}`}>
+                    <div className={`${styles['flex-center']} ${styles['justify-center']}`}><img className={styles['img']} src={img} /></div>
+                    <div className={`${styles['flex-center']} ${styles['justify-center']} ${styles['flex-colum']}`}>
+                      <h5 className={`${styles['label-gry-clr']} ${styles['fs-12']} ${styles['t-c']}`}>{name}</h5>
+                      <span className={`${styles['light-gry-clr']} ${styles['t-c']}`}>
+                        <span className={styles['fs-20']}>{price}</span> <span clsName={styles['fs-14']}> {cur}</span>
+                      </span>
+                    </div>
+                    <div className={`${styles['t-c']} ${styles['add-cart-btn']}`}><button id={listing_id} data-wish-id={wishlist_id} data-cart-res={true} className={`${styles['fp-btn']} ${styles['fp-btn-primary']} ${styles['small-btn']}`} onClick={addToCart}>{WISH_LIST_PAGE.ADD_TO_CART_BTN}</button></div>
                   </div>
                 )
               })
