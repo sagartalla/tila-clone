ea14af1e47c71e38b32e1f558b19d8707be56470
sagar <sagar.talla@fptechscience.com>
2018-08-14 15:02:06 +0530

    Fix pdp page

--- components/Product/includes/ProductDetails.js
+++ components/Product/includes/ProductDetails.js
@@ -10,7 +10,7 @@ import { mergeCss } from '../../../utils/cssUtil';
 import { languageDefinations } from '../../../utils/lang';
 import { PanelGroup,Panel, Heading, Body, Title } from 'react-bootstrap';
 const styles = mergeCss('components/Product/product');
-const { PDP } = languageDefinations();
+const { PDP_PAGE } = languageDefinations();
 
 const ProductDetails = ({ details, keyfeatures, isPreview }) => {
   return (
@@ -20,7 +20,7 @@ const ProductDetails = ({ details, keyfeatures, isPreview }) => {
           ?
           <div className={styles['border-b']}>
             <h4 className={`${styles['fs-12']} ${styles['fontW600']} ${styles['mb-5']} ${styles['mt-0']} ${styles['flx-space-bw']}`}>
-              <span>{PDP.PRODUCT_DETAILS}</span>
+              <span>{PDP_PAGE.PRODUCT_DETAILS}</span>
               <SVGCompoent clsName={`${styles['expand-icon']}`} src="icons/common-icon/down-arrow-circle" />
             </h4>
             <p className={`${styles['fs-12']} ${styles['fontW300']} ${styles['sub-decryption']}`}>{details}</p>
@@ -68,7 +68,7 @@ const ProductDetails = ({ details, keyfeatures, isPreview }) => {
             </h4>
           }
           body={
-            
+
           }
         /> */}
       </div>
