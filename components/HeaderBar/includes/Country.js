417d279ea19b31a4bd9c4d582cb10ecc52c89cd2
sagar <sagar.talla@fptechscience.com>
2018-08-17 12:43:28 +0530

    Fix sag

--- components/HeaderBar/includes/Country.js
+++ components/HeaderBar/includes/Country.js
@@ -15,12 +15,12 @@ const cookies = new Cookies();
 
 const countriesData = {
   SAU: {
-    img: '/static/img/bg-img/SAU.png',
+    img: '/static/img/bg-img/ksa.png',
     name: 'SAU',
     id: 'SAU',
   },
   ARE: {
-    img: '/static/img/bg-img/ARE.png',
+    img: '/static/img/bg-img/uae.png',
     name: 'ARE',
     id: 'ARE',
   }
