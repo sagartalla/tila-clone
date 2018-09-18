fa8b8647fd6958776523356029ed03d1cb6f452d
sagar <sagar.talla@fptechscience.com>
2018-09-18 20:01:51 +0530

    Change domain

--- constants/index.js
+++ constants/index.js
@@ -6,24 +6,24 @@ export default {
     mediaDomain: 'https://static-dev.tila.com',
     sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
     customerHelp: 'https://fptsuae.custhelp.com/app/faq/faq',
-    custhelpDomain: 'https://fptsuae--tst1.custhelp.com'
+    custhelpDomain: 'https://fptsuae.custhelp.com'
   },
   dev: {
     mediaDomain: 'https://static-dev.tila.com',
     sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
     customerHelp: 'https://fptsuae.custhelp.com/app/faq/faq',
-    custhelpDomain: 'https://fptsuae--tst1.custhelp.com'
+    custhelpDomain: 'https://fptsuae.custhelp.com'
   },
   staging: {
     mediaDomain: 'https://static-dev.tila.com',
     sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
     customerHelp: 'https://fptsuae.custhelp.com/app/faq/faq',
-    custhelpDomain: 'https://fptsuae--tst1.custhelp.com'
+    custhelpDomain: 'https://fptsuae.custhelp.com'
   },
   production: {
     mediaDomain: 'https://static-dev.tila.com',
     sellerPlatform: 'http://sellermarket-dev.fptechscience.com/',
     customerHelp: 'https://fptsuae.custhelp.com/app/faq/faqs',
-    custhelpDomain: 'https://fptsuae--tst1.custhelp.com'
+    custhelpDomain: 'https://fptsuae.custhelp.com'
   }
 }[config.publicRuntimeConfig.env || 'dev'];
