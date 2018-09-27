9c93538afc5b2b0b4bfad2a311ed5905a7363aa2
sagar <sagar.talla@fptechscience.com>
2018-09-27 11:40:15 +0530

    Change mega menu link

--- store/megamenu/api.js
+++ store/megamenu/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 import constants from '../helper/constants';
 
 //TODO accept params
-const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/1.0.7/KSA/tree?lang=EN`);
+const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/tree?countryCode=ANY&lang=EN`);
 
 export default { getMegamenu };
