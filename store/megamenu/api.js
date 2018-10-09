be0bbb04dd4f5fa3fa43653bab4551a529311b80
sagar <sagar.talla@fptechscience.com>
2018-10-09 13:16:06 +0530

    Fix megamenu

--- store/megamenu/api.js
+++ store/megamenu/api.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import axios from 'axios';
 import constants from '../helper/constants';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
 //TODO accept params
-const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/tree?countryCode=ANY&lang=EN`);
+const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/tree?countryCode=${cookies.get('country') || 'SAU'}&lang=EN`);
 
 export default { getMegamenu };
