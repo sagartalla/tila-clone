14ac6fefc52823b09840fc167a64b074d74a5b74
sagar <sagar.talla@fptechscience.com>
2018-10-30 14:31:55 +0530

    Fix langaugage

--- store/megamenu/api.js
+++ store/megamenu/api.js
@@ -5,6 +5,6 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 //TODO accept params
-const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/tree?countryCode=${cookies.get('country') || 'SAU'}&lang=EN`);
+const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/tree?countryCode=${cookies.get('country') || 'SAU'}&lang=${cookies.get('language')}`);
 
 export default { getMegamenu };
