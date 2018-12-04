68fd59efab009439e5bba8609ebd816e46a9daa7
sagar <sagar.talla@fptechscience.com>
2018-12-04 15:39:59 +0530

    Update command sh

--- deploy/commands.sh
+++ deploy/commands.sh
@@ -1,22 +1,2 @@
-
-#!/usr/bin/env bash
-set -e
-
-# Read environment variables from file and export them.
-file_env() {
-	while read -r line || [[ -n $line ]]; do
-		export $line
-	done < "$1"
-}
-
-FILE="/code/ecs_helper/environment_vars"
-
-#if file exists then export enviroment variables
-if [ -f $FILE ]; then
-	file_env $FILE
-fi
-
-exec "$@"
-
 service nginx start
 npm run start:ecs
