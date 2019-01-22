a7bff1eb3cdc85b0e85ee2c698fc91996a03d9d2
siddardha.raju <siddardha.raju@fptechscience.com>
2019-01-22 17:37:59 +0530

    Alerts changes

--- components/Cam/Wishlist/includes/WishlistBody.js
+++ components/Cam/Wishlist/includes/WishlistBody.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import SVGComponent from '../../../common/SVGComponet';
 import { languageDefinations } from '../../../../utils/lang/';
-
 import { mergeCss } from '../../../../utils/cssUtil';
+
 const styles = mergeCss('components/Cam/Wishlist/wishlist');
 
-const WishlistBody = props => {
+
+
+const WishlistBody = (props) => {
   const { data, deleteItem, addToCart } = props;
   const { WISH_LIST_PAGE } = languageDefinations();
+
+  const getPriceAlert = (a, b, cur) => {
+    if (a === b) return null;
+    const percentage = Math.floor(((a - b) / b) * 100);
+    let str = '';
+    if (percentage > 0) {
+      str = (
+        <span className={`${styles['thick-red-clr']} ${styles.flex}`}>
+          <SVGComponent clsName={`${styles['alert-icon']}`} src="icons/increase/increase" />
+          {`Price Increased by ${a - b} ${cur} (${percentage}%)`}
+        </span>
+      );
+    } else {
+      str = (
+        <span className={`${styles['success-green']} ${styles.flex}`}>
+          <SVGComponent clsName={`${styles['alert-icon']}`}src="icons/decrease/decrease" />
+          {`Price Decreased by ${a - b} ${cur} (${percentage}%)`}
+        </span>
+      );
+    }
+    return str;
+  };
 
   return (
     <div>
@@ -23,7 +47,7 @@ const WishlistBody = props => {
       <div className={`${styles['box']}`}>
         {
           data.length > 0 && data.map((item, index) => {
-            const { wishlist_id, listing_id, brand_name, name, img, price, cur } = item;
+            const { wishlist_id, listing_id, brand_name, name, img, price, cur, wishlisted_price } = item;
             return (
               <div key={index} className={`${styles['thick-border-btm']} ${styles['p-30-20']} ${styles['mb-wishlist-part']}`}>
                 <Row className={styles['m-m-0']}>
@@ -39,11 +63,15 @@ const WishlistBody = props => {
                       </div>
                     </Col>
                     <Col md={4} xs={4} className={styles['m-p-0']}>
-                      <div className={`${styles['relative']} ${styles['flex']} ${styles['price-details']}`}>
+                      <div className={`${styles['relative']} ${styles['flex']} ${styles['flex-colum']} ${styles['price-details']}`}>
                         <span id={wishlist_id} className={`${styles['absolute']} ${styles['delete-icon-part']}`} onClick={deleteItem}>
                           <SVGComponent clsName={`${styles['delete-icon']}`} src="icons/delete-icon/delete-icon" />
                         </span>
-                        <h4 className={`${styles['fontW600']} ${styles['light-gry-clr']} ${styles['mt-25']} ${styles['t-c']}`}><span className={`${styles['fs-30']} ${styles['m-fs-18']}`}>{price}</span> <span className={`styles['fs-18']} ${styles['m-fs-14']}`}> {cur}</span></h4>
+                        <h4 className={`${styles['fontW600']} ${styles['light-gry-clr']} ${styles['mt-25']}`}><span className={`${styles['fs-30']} ${styles['m-fs-18']}`}>{price}</span> <span className={`styles['fs-18']} ${styles['m-fs-14']}`}> {cur}</span></h4>
+                        {getPriceAlert(price, wishlisted_price, cur)}
+                        <span className={`${styles['thick-gry-clr']}`}>
+                          Item was {wishlisted_price} {cur} when added to Wish List
+                        </span>
                       </div>
                     </Col>
                   </Col>
--- store/cam/wishlist/selectors.js
+++ store/cam/wishlist/selectors.js
@@ -5,7 +5,7 @@ const getWishListResults = (store) => {
     const img_url = constants.mediaDomain;
     const newData = [];
 
-    data.map((item) => {
+    data.forEach((item) => {
       // const variant = Object.keys(item.product_details.product_details_vo.cached_variant)[0];
       // const variant_info = item.variant_preferred_listings[variant][0];
 
@@ -19,11 +19,12 @@ const getWishListResults = (store) => {
         variant_id: variant_info.variant_id,
         name: item.product_details.product_details_vo.cached_product_details.attribute_map.calculated_display_name.attribute_values[0].value,
         brand_name: item.product_details.catalog_details.attribute_map.brand.attribute_values[0].value,
-        img: img_url + '/' +  item.product_details.product_details_vo.cached_product_details.media.gallery_media[0].url,
+        img: img_url + '/' + item.product_details.product_details_vo.cached_product_details.media.gallery_media[0].url,
         cur: variant_info.selling_price_currency,
         price: variant_info.selling_price,
-      })
-    })
+        wishlisted_price: item.wishlisted_price,
+      });
+    });
 
     return newData.reverse();
   }
