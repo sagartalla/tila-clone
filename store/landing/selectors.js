f5427d7490c01ddc061f636ea6079b0192402a5e
Avinash Nallamothu <avinashnallamothu@FPTS-Avinash.local>
2019-02-27 11:45:22 +0530

    removing merge conflicts

--- store/landing/selectors.js
+++ store/landing/selectors.js
@@ -5,12 +5,7 @@ const getPages = (store) => {
 }
 
 const getElectronicsPage = (store) => {
-<<<<<<< Updated upstream
   return JSON.parse(_.find(store.landingReducer.data.content, { pageTypeValue: 'Electronics' }).json);
-=======
-  console.log(store.landingReducer.data)
-  return JSON.parse(_.find(store.landingReducer.data.content, { page_type_value: 'Electronics' }).json);
->>>>>>> Stashed changes
 }
 
 const getFashionPage = (store) => {
