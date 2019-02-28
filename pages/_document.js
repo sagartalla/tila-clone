da3f3a0e746cc780a7c0e5bff778431b71bd6d21
sagar <sagar.talla@fptechscience.com>
2019-02-28 12:33:42 +0530

    Fix gym

--- pages/_document.js
+++ pages/_document.js
@@ -9,13 +9,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           {/*<!-- Google Tag Manager -->*/}
-          <Script>
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M68MT36');
-          </Script>
+          <script src="/static/scripts/googleTagManager.js"></script>
           {/*<!-- Adobe Launch Tags -->*/}
           <script src="//assets.adobedtm.com/launch-ENc0358fe6617e4066a1c1c0ecff96f2e5-development.min.js" async></script>
           {/*<!-- End Adobe Launch Tags -->*/}
