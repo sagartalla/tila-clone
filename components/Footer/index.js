86c00e49e1003e03e7c462c77904adf2742e4c9d
sagar <sagar.talla@fptechscience.com>
2019-02-28 19:45:05 +0530

    Add links

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,6 +6,34 @@ const styles = mergeCss('components/Footer/footer');
 import { languageDefinations } from '../../utils/lang';
 const {FOOTER_PAGE} = languageDefinations();
 
+
+const f = [{"Women Clothing":	"https://storefront-stage.fptechscience.com/srp/Clothing-910?categoryTree=true&isListed=true"},
+{"Men Clothing":	"https://storefront-stage.fptechscience.com/srp/Clothing-899?categoryTree=true&isListed=true"},
+{"Kid's Fashion":	"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=Kids"},
+{"Watches":	"https://storefront-stage.fptechscience.com/srp/watches-1128/?isListed=false&language=en&search=Watch"},
+{"Jewellery":	"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=Jewellery"},
+{"Mens Footwear":	"https://storefront-stage.fptechscience.com/srp/Footwear-921?categoryTree=true&isListed=false&language=en"},
+{"Womens Footwear":	"https://storefront-stage.fptechscience.com/srp/Footwear-921?categoryTree=true&isListed=false&language=en"},
+{"Bags":	"https://storefront-stage.fptechscience.com/srp/Backpack-926?categoryTree=true&isListed=false&language=en"}];
+
+const e = [{"Mobiles":"https://storefront-stage.fptechscience.com/srp?search=Mobiles&language=en&isListed=true"},
+{"Tablets":"https://storefront-stage.fptechscience.com/srp/Tablets-877?categoryTree=true&isListed=false&language=en"},
+{"Laptops":"https://storefront-stage.fptechscience.com/srp/laptop-1173/?search=Laptops&language=en&isListed=true"},
+{"Home Appliances":"https://storefront-stage.fptechscience.com/srp?search=Home%20Appliances&language=en&isListed=true"},
+{"Cameras":"https://storefront-stage.fptechscience.com/srp?search=Camera&language=en&isListed=true"},
+{"Television":"https://storefront-stage.fptechscience.com/srp/Televisions-878?categoryTree=true&isListed=true"},
+{"Speakers":"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=Speaker"},
+{"Storage Devices":"https://storefront-stage.fptechscience.com/srp?search=Storage&language=en&isListed=true"}];
+
+const l = [{"Travel Acessories":	"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=travel%20accessories"},
+{"Photo Frame":	"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=Photo%20Frame"},
+{"Light":	"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=Light"}];
+
+const b = [{"Perfumes":	"https://storefront-stage.fptechscience.com/srp?disableSpellCheck=true&isListed=false&language=en&search=perfume"},
+{"Eyewear":	"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=eyewear"},
+{"Men's grooming":	"https://storefront-stage.fptechscience.com/srp?isListed=false&language=en&search=mens%20grooming"},
+{"Personal Care":	"https://storefront-stage.fptechscience.com/srp/Personal%20Care-969?categoryTree=true&isListed=false&language=en"}];
+
 const FooterBar = props => (
   <div className={`${styles['footer-container']} ${styles['mt-25']} hidden-xs`}>
     <div className={`${styles['footer-container-inn']} ${styles['pt-40']} ${styles['pb-40']}`}>
@@ -67,14 +95,15 @@ const FooterBar = props => (
               <span className={styles['pointer']}>Fashion</span>
             </h4>
             <ul className={`${styles['pl-30']} ${styles['lne-ht2']}`}>
-              <li><span className={styles['pointer']}>{`${FOOTER_PAGE.WOMENS} ${FOOTER_PAGE.FASHION}`}</span></li>
+              {f.map((i) => <li><a href={Object.values(i)[0]} target="_blank"><span className={styles['pointer']}>{Object.keys(i)[0]}</span></a></li>)}
+              {/*<li><span className={styles['pointer']}>{`${FOOTER_PAGE.WOMENS} ${FOOTER_PAGE.FASHION}`}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.MENS} ${FOOTER_PAGE.FASHION}`}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.GIRLS} ${FOOTER_PAGE.FASHION}`}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.BOYS} ${FOOTER_PAGE.FASHION}`}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.WATCHES}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.JEWELLERY}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.WOMENS} ${FOOTER_PAGE.HANDGBAGS}`}</span></li>
-              <li><span className={styles['pointer']}>{`${FOOTER_PAGE.MENS} ${FOOTER_PAGE.EYEWEAR}`}</span></li>
+              <li><span className={styles['pointer']}>{`${FOOTER_PAGE.MENS} ${FOOTER_PAGE.EYEWEAR}`}</span></li>*/}
             </ul>
           </Col>
           <Col md={3} sm={3} className={styles['ipad-pr-0']}>
@@ -83,14 +112,15 @@ const FooterBar = props => (
               <span className={styles['pointer']}>{FOOTER_PAGE.ELECTRONICS}</span>
             </h4>
             <ul className={`${styles['pl-30']} ${styles['lne-ht2']}`}>
-              <li><span className={styles['pointer']}>{FOOTER_PAGE.MOBILES}</span></li>
+              {e.map((i) => <li><a href={Object.values(i)[0]} target="_blank"><span className={styles['pointer']}>{Object.keys(i)[0]}</span></a></li>)}
+              {/*<li><span className={styles['pointer']}>{FOOTER_PAGE.MOBILES}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.TABLETS}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.LAPTOPS}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.HOME_APLLIANCES}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.CAMERA},${FOOTER_PAGE.PHOTO} & ${FOOTER_PAGE.VIDEO}`}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.TELEVISIONS}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.HEADPHONES}</span></li>
-              <li><span className={styles['pointer']}>{FOOTER_PAGE.VIDEO_GAMES}</span></li>
+              <li><span className={styles['pointer']}>{FOOTER_PAGE.VIDEO_GAMES}</span></li>*/}
             </ul>
           </Col>
           <Col md={2} sm={2} className={styles['ipad-pr-0']}>
@@ -99,14 +129,15 @@ const FooterBar = props => (
               <span className={styles['pointer']}>Lifestyle</span>
             </h4>
             <ul className={`${styles['pl-30']} ${styles['lne-ht2']}`}>
-              <li><span className={styles['pointer']}>{FOOTER_PAGE.HOME_DECOR}</span></li>
+              {l.map((i) => <li><a href={Object.values(i)[0]} target="_blank"><span className={styles['pointer']}>{Object.keys(i)[0]}</span></a></li>)}
+              {/*<li><span className={styles['pointer']}>{FOOTER_PAGE.HOME_DECOR}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.KITCHEN} & ${FOOTER_PAGE.DINING}`}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.BATH}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.HOME_APLLIANCES}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.TOOLS} & ${FOOTER_PAGE.HOME_IMPROVE}`}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.AUDIO} & ${FOOTER_PAGE.VIDEO}`}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.FURNITURE}</span></li>
-              <li><span className={styles['pointer']}>{`${FOOTER_PAGE.PATIO}, ${FOOTER_PAGE.LAWN} & ${FOOTER_PAGE.GARDEN}`}</span></li>
+              <li><span className={styles['pointer']}>{`${FOOTER_PAGE.PATIO}, ${FOOTER_PAGE.LAWN} & ${FOOTER_PAGE.GARDEN}`}</span></li>*/}
             </ul>
           </Col>
           <Col md={2} sm={2} className={styles['ipad-pr-0']}>
@@ -115,13 +146,14 @@ const FooterBar = props => (
               <span className={styles['pointer']}>{`${FOOTER_PAGE.BEAUTY} & ${FOOTER_PAGE.MAKEUP}`}</span>
               </h4>
             <ul className={`${styles['pl-30']} ${styles['lne-ht2']}`}>
-              <li><span className={styles['pointer']}>{FOOTER_PAGE.FRAGRANCE}</span></li>
+              {b.map((i) => <li><a href={Object.values(i)[0]} target="_blank"><span className={styles['pointer']}>{Object.keys(i)[0]}</span></a></li>)}
+              {/*<li><span className={styles['pointer']}>{FOOTER_PAGE.FRAGRANCE}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.MAKEUP}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.HAIRCARE}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.SKINCARE}${FOOTER_PAGE.AUDIO} & ${FOOTER_PAGE.VIDEO}`}</span></li>
               <li><span className={styles['pointer']}>{FOOTER_PAGE.PERSONAL_CARE}</span></li>
               <li><span className={styles['pointer']}>{`${FOOTER_PAGE.TOOLS} & ${FOOTER_PAGE.ACCESSORIES}`}</span></li>
-              <li><span className={styles['pointer']}>{`${FOOTER_PAGE.MENS} ${FOOTER_PAGE.GROOMING}`}</span></li>
+              <li><span className={styles['pointer']}>{`${FOOTER_PAGE.MENS} ${FOOTER_PAGE.GROOMING}`}</span></li>*/}
             </ul>
           </Col>
 
