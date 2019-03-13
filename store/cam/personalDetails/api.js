5c78635ec411e95b5b77f9b0e810f4c1f5b4d441
avinash <avinash.nallamothu@fptechscience.com>
2019-03-13 14:43:44 +0530

    adding url to constants

--- store/cam/personalDetails/api.js
+++ store/cam/personalDetails/api.js
@@ -20,7 +20,7 @@ const changePassword = (body) => {
 };
 
 const forgotPassword = (body) => {
-  return axios.post('https://apigateway-dev.tila.com/cms/api/v1/user/password/forgot', body).then(({data}) => {
+  return axios.post(`${constants.CMS_API_URL}/api/v1/user/password/forgot`, body).then(({data}) => {
     return data;
   }).catch((error) => {
     return error.response.data;
