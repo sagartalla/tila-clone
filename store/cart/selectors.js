ace34523165ba7b3bb510b442b1a43c1abb29c26
Mayur Ray <shiremen@gmail.com>
2019-03-18 19:26:08 +0530

    warranty_selectors_change

--- store/cart/selectors.js
+++ store/cart/selectors.js
@@ -37,7 +37,7 @@ const getCartResults = (store) => {
           shipping: item.listing_info.shipping,
           catalogId: item.product_details.catalog_details.catalog_id,
           itemType: item.product_details.catalog_details.item_type_name,
-          warranty_duration: item.tree.finance.filter((data)=> data.isLeaf === true)[0].warranty ? JSON.parse(item.tree.finance.filter((data)=> data.isLeaf === true)[0].warranty) : null,
+          warranty_duration: item.listing_info.warranty_policy.policies.TILA,
           warranty: _.groupBy(item.listing_info.warranty_details, 'type')['MANUFACTURER'] || [{}],
           discount: item.listing_info.pricing && item.listing_info.pricing.discount_per_mrp,
           mrp: item.listing_info.pricing && item.listing_info.pricing.mrp,
