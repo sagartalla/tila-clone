8e194ba10f0b088e89668bdb99d81599997193d8
avinash <avinash.nallamothu@fptechscience.com>
2019-03-30 17:25:09 +0530

    adding pagination in order and common component

--- store/cam/orders/api.js
+++ store/cam/orders/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import constants from '../../helper/constants';
 
-const getOrderHistory = () => axios.get(`${constants.ORDERS_API_URL}/api/v1/customer/order/history?grouped=true`);
+const getOrderHistory = (pageSize) => axios.get(`${constants.ORDERS_API_URL}/api/v1/customer/order/history?grouped=true&page=${pageSize}`);
 
 export default { getOrderHistory };
