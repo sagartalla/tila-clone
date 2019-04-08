fa0f2f1a0faa5b33d8937401a9731dd4c368a916
sagar <sagar.talla@fptechscience.com>
2019-04-08 14:46:15 +0530

    Fix api

--- store/cam/userVault/api.js
+++ store/cam/userVault/api.js
@@ -23,7 +23,7 @@ const makeCardDefaultApi = (card_token) => {
 }
 
 const deleteCardApi = (card_token) => {
-  return axios.delete(`${constants.VAULT_API_URL}/api/v1/vault/card/` + card_token).then(({ data }) => {
+  return axios.delete(`${constants.VAULT_API_URL}//api/v1/user/wallet/deleteCard/${card_token}`).then(({ data }) => {
     //after successfully added, get all card details again.
     return getCardResultsApi();
   });
