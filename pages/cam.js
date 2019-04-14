ee39891ce6d22f3f2de489fdca9e8ff19e4dccfc
sagar <sagar.talla@fptechscience.com>
2019-04-15 00:08:10 +0530

    CAM AR

---
+++ pages/cam.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import NoSSR from 'react-no-ssr';
+import withRedux from 'next-redux-wrapper';
+import { bindActionCreators } from 'redux';
+import { configureUrlQuery } from 'react-url-query';
+import createHistory from 'history/createBrowserHistory';
+
+import makeStore from '../store';
+
+import Base, { baseActions } from './base';
+import Layout from '../layout/main';
+import Cam from '../components/Cam';
+
+class CamPage extends Base {
+  pageName = 'CAM';
+  render(){
+    const { url } = this.props;
+    return (
+      <Layout>
+        <NoSSR>
+          <Cam tabDetails={url.query.tabDetails} query={url.query}/>
+        </NoSSR>
+      </Layout>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      ...baseActions,
+    },
+    dispatch,
+  )
+
+export default withRedux(makeStore, null, mapDispatchToProps)(CamPage);
