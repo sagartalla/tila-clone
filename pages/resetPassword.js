dd6c93ca081e31846d709cf4bb434bd6874c3582
Susmitha <susmitha@fptechscience.com>
2019-04-15 15:22:15 +0530

    Forgot password change

--- pages/resetPassword.js
+++ pages/resetPassword.js
@@ -9,34 +9,36 @@ class ResetPasswordPage extends Base {
   constructor(props) {
     super(props);
     this.state = {
-      // token: null,
+      token: null,
     };
   }
   pageName = 'RESETPASSWORD';
-  // componentWillMount() {
-  //   let token = window.location.pathname.split('/');
-  //   token = token[token.length - 1];
-  //   this.setState({
-  //     token,
-  //   });
-  // }
+  componentWillMount() {
+    debugger;
+    let token = window.location.pathname.split('/');
+    token = token[token.length - 1];
+    this.setState({
+      token,
+    });
+    console.log('token', token);
+  }
 
   render() {
-    // const { token } = this.state;
+    const { token } = this.state;
     return (
     // TODO >>> token should be dynamic
-    // <ResetPassword token={token}/>
-      <ResetPassword token="ab7eb0775db645ff9b96af619a6e97aa" />
+      <ResetPassword token={token} />
+    // <ResetPassword token="ab7eb0775db645ff9b96af619a6e97aa" />
     );
   }
 }
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      ...baseActions,
-    },
-    dispatch,
-  );
+// const mapDispatchToProps = dispatch =>
+//   bindActionCreators(
+//     {
+//       ...baseActions,
+//     },
+//     dispatch,
+//   );
 
-export default withRedux(makeStore, null, mapDispatchToProps)(ResetPasswordPage);
+export default ResetPasswordPage;
