978acfb8d6b0c3bbba7f5aefbb45845ccb0a9b12
ravikumar <ravikiran.bandla@fptechscience.com>
2019-04-23 12:48:53 +0530

    cod and add to cart changes

--- components/Product/includes/TitleInfo.js
+++ components/Product/includes/TitleInfo.js
@@ -144,7 +144,7 @@ class TitleInfo extends Component {
             ))
           } */}
         </div>
-        <h1 className={`${styles['fs-18']} ${styles.fontW700} ${styles['black-color']}`}>{title}</h1>
+        <h1 className={`${styles['fs-18']} ${styles.fontW700} ${styles['black-color']} ${styles['mt-5']} ${styles['mb-0']}`}>{title}</h1>
         {
           isPreview
             ?
@@ -162,15 +162,16 @@ class TitleInfo extends Component {
             null
             :
             <div className={`${styles['flex-center']} ${styles['checkout-instantly']} ${styles['pt-10']}`}>
-              <div className={`${styles.flex}`}>
-                {totalInventoryCount > 0 &&
-                  <a className={`${styles['fp-btn']} ${styles['fp-btn-default']} ${styles['fs-14']} ${styles['small-btn']} ${styles['checkout-instant-btn']}`} onClick={this.checkoutInstantHandler}>{PDP_PAGE.CHECKOUT_INSTANT}</a>}
-              </div>
-              <div>
+              {totalInventoryCount > 0 &&
+                <div className={`${styles.flex} ${styles['pr-10']}`}>
+                 <a className={`${styles['fp-btn']} ${styles['fp-btn-default']} ${styles['fs-14']} ${styles['small-btn']} ${styles['checkout-instant-btn']}`} onClick={this.checkoutInstantHandler}>{PDP_PAGE.CHECKOUT_INSTANT}</a>
+                </div>
+              }
+              <div className={styles['flex']}>
                 {
                   totalInventoryCount < 5
                     ?
-                    <span className={`${styles.flex} ${styles['fs-12']} ${styles['google-clr']} ${styles.fontW600}`}>
+                    <span className={`${styles.flex} ${styles['fs-12']} ${styles['google-clr']} ${styles['pr-10']} ${styles.fontW600}`}>
                       {
                         totalInventoryCount === 0
                           ?
@@ -180,9 +181,8 @@ class TitleInfo extends Component {
                         }
                     </span>
                     :
-                    null
+                    null 
                 }
-
                 <span className={`${styles.flex} ${styles['fs-12']}`}>{PDP_PAGE.COD_AVAILABLE}</span>
               </div>
 
