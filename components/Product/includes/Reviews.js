67b137ba8528be382ba51a3ccea7329c9aa745bb
Mayur Ray <shiremen@gmail.com>
2019-05-23 18:05:31 +0530

    Reviews_login

--- components/Product/includes/Reviews.js
+++ components/Product/includes/Reviews.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
-import { Grid, Row, Col, Tabs, Tab, ProgressBar } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import Theme from '../../helpers/context/theme';
 import { selectors, actionCreators } from '../../../store/product';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Modal } from 'react-bootstrap';
-import SVGComponent from '../../common/SVGComponet';
 import ReviewThankYou from './ReviewThankYou';
 import { languageDefinations } from '../../../utils/lang';
 const { PDP_PAGE } = languageDefinations();
@@ -58,10 +54,15 @@ class Review extends Component {
   }
   popupClosed = () => {
     this.setState({
-      openModal: false,
+      openModal: true,
     });
   }
-  submituserreview = (reviewObj) => {    
+  popupOpened = () => {
+    this.setState({
+      openModal: false,
+    })
+  }
+  submituserreview = (reviewObj) => {
     this.props.submitUserReview(reviewObj).then(() => {
       this.setState({
         showReviews: false,
@@ -69,21 +70,21 @@ class Review extends Component {
     });
   }
 
-  renderReviewDetails = (reviewData,categoryType) => {
-    return reviewData.map((data,i) => {
+  renderReviewDetails = (reviewData, categoryType) => {
+    return reviewData.map((data, i) => {
       return (
-        <Col md={12} key={'review_'+i}>
+        <Col md={12} key={'review_' + i}>
           <Col md={4}>
             <div className={`${styles['mb-10']} ${styles['flex-center']}`}>
-               <div className={`${styles['profile-img']} ${styles['flex']}`}>
-                 <span></span>
-               </div>
+              <div className={`${styles['profile-img']} ${styles['flex']}`}>
+                <span></span>
+              </div>
               <div className={`${styles['pl-20']} ${styles['thick-gry-clr']}`}>
                 <h5 className={`${styles['mb-0']} ${styles['fontW600']}`}>{data.reviewer_name}</h5>
                 <div className={`${styles['flex']} ${styles['review-start-inn']} ${styles['pb-5']} ${styles['pt-5']}`}>
                   <StarRating
                     interactive={false}
-                    count= {5}
+                    count={5}
                     rating={data.ratings}
                     clsStyl={{ width: '15px', marginRight: '5px' }}
                   />
@@ -93,7 +94,7 @@ class Review extends Component {
           </Col>
           <Col md={8} className={styles['p-0']}>
             <p className={`${styles['fs-12']} ${styles['thick-gry-clr']}`}>
-              { data.comment }
+              {data.comment}
             </p>
           </Col>
         </Col>
@@ -120,81 +121,85 @@ class Review extends Component {
                   <Col md={6}></Col>
                 </Col>
                 <Col md={6} className={styles['flex-center']}>
-                    <Col md={6} className={styles['t-c']}>
-                      <span>{PDP_PAGE.SHARE_EXPIERENCE}</span>
-                    </Col>
-                    <Col md={6} >
-                      <div className={`${styles['flex']} ${styles['review-start']} ${styles['pb-10']}`}>
-                        <StarRating interactive={false} total={5} />
-                      </div>
-                      <a
+                  <Col md={6} className={styles['t-c']}>
+                    <span>{PDP_PAGE.SHARE_EXPIERENCE}</span>
+                  </Col>
+                  <Col md={6} >
+                    <div className={`${styles['flex']} ${styles['review-start']} ${styles['pb-10']}`}>
+                      <StarRating interactive={false} total={5} />
+                    </div>
+                    <a
                       className={`${styles['fp-btn']} ${styles['fp-btn-default']} ${styles['wrt-btn']} ${styles['small-btn']}`}
-                      onClick={this.toggleReviewModal} 
-                      >
+                      onClick={this.toggleReviewModal}
+                    >
                       {PDP_PAGE.WRITE_REVIEW}
-                     </a>
+                    </a>
                   </Col>
                 </Col>
-                </div>
-                <React.Fragment>
-                  {
-                    reviewData.length ? this.renderReviewDetails(reviewData,categoryType) : <div> {PDP_PAGE.NO_REVIEWS_AVAILABLE} </div>
-                  }
-                </React.Fragment>
-                <div>
-                  {
-                    openModal ?
+              </div>
+              <React.Fragment>
+                {
+                  reviewData.length ? this.renderReviewDetails(reviewData, categoryType) : <div> {PDP_PAGE.NO_REVIEWS_AVAILABLE} </div>
+                }
+              </React.Fragment>
+              <div>
+                {
+                  openModal ?
                     // <AuthWrapper
-                  //     popupClosed={this.popupClosed}
-                  //     >
-                  //     <Modal
-                  //       {...this.props}
-                  //       show={openModal}
-                  //       onHide={this.toggleReviewModal}
-                  //       dialogClassName="custom-modal"
-                  //       >
-                  //     <Modal.Header
-                  //       closeButton
-                  //       className={`${styles['modal-headerStyl']}`}
-                  //     >
-                  //       <Modal.Title>{PDP_PAGE.SHARE_YOUR_EXPERIENCE}</Modal.Title>
-                  //     </Modal.Header>
-                  //    <Modal.Body>
-                  //      <ReviewFeedBackModal
-                  //       catalogObj={catalogObj}
-                  //       feedbackSubmit={this.submituserreview}
-                  //      />
-                  //    </Modal.Body>
-                  //   </Modal>
-                  // </AuthWrapper>
-                  <React.Fragment>
-                    <div onClick={this.closeSlider} className={openModal ? `${styles['modalContainer']} ${styles['showDiv']}` : `${styles['modalContainer']} ${styles['hideDiv']}`}>
-                      <div className={`${styles['disabled']}`} />
-                    </div>
-                    <div className={`${styles['overflow-y-auto']} ${styles['p-30']} ${openModal ? `${styles['openModal']}` : `${styles['closeModal']}`}`}>
-                      <div className={styles['p-40']}>
-                        <h4 className={`${styles['flex']} ${styles['justify-flex-end']} ${styles['m-0']} ${styles['mb-20']}`}>
-                          <a onClick={this.toggleReviewModal} className={`${styles['fs-22']} ${styles['black-color']}`}>X</a>
-                        </h4>
-                        <div>
-                          {showReviews ?
-                            <ReviewFeedBackModal
-                              catalogObj={catalogObj}
-                              titleInfo={titleInfo}
-                              feedbackSubmit={this.submituserreview}
-                            />
-                          : 
-                            <ReviewThankYou closePopup={this.popupClosed} />
-                          }
+                    //   popupClosed={this.popupClosed}
+                    //   >
+                    //     <Modal
+                    //       {...this.props}
+                    //       show={openModal}
+                    //       onHide={this.toggleReviewModal}
+                    //       dialogClassName="custom-modal"
+                    //       >
+                    //     <Modal.Header
+                    //       closeButton
+                    //       className={`${styles['modal-headerStyl']}`}
+                    //     >
+                    //       <Modal.Title>{PDP_PAGE.SHARE_YOUR_EXPERIENCE}</Modal.Title>
+                    //     </Modal.Header>
+                    //    <Modal.Body>
+                    //      <ReviewFeedBackModal
+                    //       catalogObj={catalogObj}
+                    //       feedbackSubmit={this.submituserreview}
+                    //      />
+                    //    </Modal.Body>
+                    //   </Modal>
+                    // </AuthWrapper>
+                    <React.Fragment>
+                      <AuthWrapper
+                        popupClosed={this.popupClosed}
+                        popupOpened={this.popupOpened}
+                      >
+                        <div onClick={this.closeSlider} className={openModal ? `${styles['modalContainer']} ${styles['showDiv']}` : `${styles['modalContainer']} ${styles['hideDiv']}`}>
+                          <div className={`${styles['disabled']}`} />
                         </div>
-                      </div>
-                    </div>
-                  </React.Fragment>
-         
-                  : null
-                  }
+                        <div className={`${styles['overflow-y-auto']} ${styles['p-30']} ${openModal ? `${styles['openModal']}` : `${styles['closeModal']}`}`}>
+                          <div className={styles['p-40']}>
+                            <h4 className={`${styles['flex']} ${styles['justify-flex-end']} ${styles['m-0']} ${styles['mb-20']}`}>
+                              <a onClick={this.toggleReviewModal} className={`${styles['fs-22']} ${styles['black-color']}`}>X</a>
+                            </h4>
+                            <div>
+                              {showReviews ?
+                                <ReviewFeedBackModal
+                                  catalogObj={catalogObj}
+                                  titleInfo={titleInfo}
+                                  feedbackSubmit={this.submituserreview}
+                                />
+                                :
+                                <ReviewThankYou closePopup={this.popupClosed} />
+                              }
+                            </div>
+                          </div>
+                        </div>
+                      </AuthWrapper>
+                    </React.Fragment>
 
-                </div>
+                    : null
+                }
+              </div>
             </div>
           )
         }
@@ -210,7 +215,7 @@ const mapStateToProps = (store) => {
   });
 };
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators (
+  return bindActionCreators(
     {
       getRatingsAndReviews: actionCreators.getRatingsAndReviews,
       submitUserReview: actionCreators.submitUserReview,
@@ -218,4 +223,4 @@ const mapDispatchToProps = (dispatch) => {
     dispatch,
   );
 };
-export default connect(mapStateToProps,mapDispatchToProps)(Review);
+export default connect(mapStateToProps, mapDispatchToProps)(Review);
