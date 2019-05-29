7f37304fb2fa07375c6a485bf362f1b7f5a63c28
siddardha.raju <siddardha.raju@fptechscience.com>
2019-05-29 18:20:42 +0530

    Changes to review page

--- components/Cam/Reviews/index.js
+++ components/Cam/Reviews/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import SVGCompoent from '../../common/SVGComponet';
 import { actionCreators, selectors } from '../../../store/ratingReviews';
 import constants from '../../../constants';
 import lang from '../../../utils/language';
 import { languageDefinations } from '../../../utils/lang/';
+import StarRating from '../../common/StarRating';
+import Button from '../../common/CommonButton';
+
 import main_en from '../../../layout/main/main_en.styl';
 import main_ar from '../../../layout/main/main_ar.styl';
 import styles_en from './reviews_en.styl';
@@ -28,18 +32,43 @@ class Reviews extends React.Component {
   render() {
     const { userReviews } = this.props;
     return (
-      <div className={`${styles.box} ${styles.flex} ${styles['flex-colum']}`}>
-        <div>
-          <h2 className={`${styles['fs-18']}`}><span className={`${styles.fontW600}`}>You have Rated & Reviewed</span> <span className={`${styles['fs-12']}`}> {userReviews.length} items</span></h2>
+      <div className={`${styles.box} ${styles.flex} ${styles['flex-colum']} ${styles['my-review']}`}>
+        <div className={`${styles['p-20-40']} ${styles['my-review-head']} ${styles['flex-center']}`}>
+          <div className={`${styles.flex}`}>
+            <SVGCompoent src="icons/illustration" clsName={styles.icon} />
+          </div>
+          <div className={`${styles['pl-30']}`}>
+            <h3 className={`${styles['fs-22']} ${styles['m-0']}`}>My Reviews & Ratings</h3>
+            <span className={`${styles['fs-12']} ${styles['dottes-gry-clr']}`}>All your reviews & ratings at one place</span>
+          </div>
+        </div>
+        <div className={styles['my-review-inn']}>
+          <h2 className={`${styles['fs-18']} ${styles['pt-15']} ${styles['pb-15']} ${styles['pl-40']} ${styles['bg-light-gray']}  ${styles['m-0']}`}>
+            <span className={`${styles.fontW600}`}>You have Rated & Reviewed</span> <span className={`${styles['fs-12']}`}> {userReviews.length} items</span>
+          </h2>
           <div>
             {userReviews.length > 0 &&
               userReviews.map(rev => (
-                <div>
+                <div key={rev.review_id} className={`${styles['pl-15']} ${styles['pr-15']} ${styles['pt-15']} ${styles['flex-center']}`}>
                   <div>
-                    <img className={styles.img} alt="" src={`${constants.mediaDomain}/${rev.images}`} />
+                    <img className={styles.icon} alt="" src={`${constants.mediaDomain}/${rev.product_image_url}`} />
                   </div>
-                  <div>
-                    <div>{rev.comment}</div>
+                  <div className={`${styles['pl-15']} ${styles['pr-15']} ${styles['pt-15']} ${styles['pb-30']} ${styles['border-b']} ${styles.width100}`}>
+                    <div className={`${styles['pb-10']} ${styles['fs-14']} ${styles.fontW600}`}>{rev.title}</div>
+                    <StarRating
+                      interactive={false}
+                      count={5}
+                      rating={rev.ratings}
+                      clsStyl={{ width: '15px', marginRight: '5px' }}
+                    />
+                    <div className={`${styles['pt-10']} ${styles['fs-12']} ${styles['dottes-gry-clr']}`}>{rev.comment}</div>
+                    {!rev.comment &&
+                      <Button
+                        id={rev.review_id}
+                        className={`${styles.flex} ${styles['preference-save']} ${styles.fontW600} ${styles['fs-10']} ${styles['text-uppercase']}`}
+                        btnText="Write a review"
+                        onClick={this.savePreferences}
+                      />}
                   </div>
                 </div>
               ))}
--- components/common/StarRating/index.js
+++ components/common/StarRating/index.js
@@ -4,96 +4,101 @@ import Star from './Star';
 
 export default class StarRating extends Component {
   state = {
-    rating:this.props.rating,
-    lastRating:this.props.rating,
-    isRating:false
+    rating: this.props.rating,
+    lastRating: this.props.rating,
+    isRating: false,
   }
-  willRate = (rating) => (e) => {
+
+  onRate = rating => (e) => {
     this.setState({
       rating,
-      isRating:true
-    })
+      lastRating: rating,
+      isRating: false,
+    });
+    const { onRate: callback } = this.props;
+    if (callback) callback(e, rating);
   }
-  onRate = (rating) => (e) => {
-    this.setState({
-      rating,
-      lastRating:rating,
-      isRating:false
-    })
-    const {onRate:callback} = this.props;
-    callback && callback(e,rating)
-  }
+
   onCancelRate = () => {
-    let { lastRating:rating } = this.state
+    const { lastRating: rating } = this.state;
     this.setState({
-        rating,
-        isRating:false
-      })
+      rating,
+      isRating: false,
+    });
+  }
+
+  willRate = rating => () => {
+    this.setState({
+      rating,
+      isRating: true,
+    });
   }
 
   render() {
-    let { total,children,interactive,clsStyl,clsName,  ...restProps} = this.props
-    let { rating, isRating } = this.state;
-    children = Children.toArray(children)
+    let {
+      total, children, interactive, clsStyl, clsName, ...restProps
+    } = this.props;
+    const { rating, isRating } = this.state;
+    children = Children.toArray(children);
 
-    let nodes = Array.apply(null,Array(total)).map((_,i) => {
-      let starProps = {
-        isActive:!isRating && rating - i >= 1,
-        willBeActive:isRating && i < rating,
-        isDisabled:!interactive
-      }
+    const nodes = Array(...Array(total)).map((_, i) => {
+      const starProps = {
+        isActive: !isRating && rating - i >= 1,
+        willBeActive: isRating && i < rating,
+        isDisabled: !interactive,
+      };
       return (
         <div
-         key={`star_${i}`}
-         onClick={interactive ? this.onRate((i + 1)) : null}
-         onMouseOver={interactive ? this.willRate((i + 1)):null}
+          key={`star_${i}`}
+          onClick={interactive ? this.onRate((i + 1)) : null}
+          onMouseOver={interactive ? this.willRate((i + 1)) : null}
         >
-        {
+          {
           children.length ? (
             React.cloneElement(children[i % children.length], starProps)
-          ):(
+          ) : (
             <Star clsStyl={clsStyl} clsName={clsName} {...starProps} />
           )
         }
 
-       </div>
-      )
-    })
-    if(interactive) {
+        </div>
+      );
+    });
+    if (interactive) {
       return (
         <div
-         style={{display:'flex'}}
-         className='react-rater'
-         onMouseOut={this.onCancelRate}
-         {...restProps}
+          style={{ display: 'flex' }}
+          className="react-rater"
+          onMouseOut={this.onCancelRate}
+          {...restProps}
         >
           { nodes }
         </div>
-      )
-    } else {
-      return (
-        <div
-        style={{display:'flex'}}
-        {...restProps}>
-          {nodes}
-        </div>
-      )
-
+      );
     }
+    return (
+      <div
+        style={{ display: 'flex' }}
+        {...restProps}
+      >
+        {nodes}
+      </div>
+    );
   }
 }
 
 StarRating.propTypes = {
-  total:PropTypes.number,
-  rating:PropTypes.number,
-  onRate:PropTypes.func,
-  onRating:PropTypes.func,
-  clsStyl:PropTypes.object
-}
+  total: PropTypes.number,
+  rating: PropTypes.number,
+  onRate: PropTypes.func.isRequired,
+  onRating: PropTypes.func.isRequired,
+  clsStyl: PropTypes.object,
+  interactive: PropTypes.bool,
+};
 
 StarRating.defaultProps = {
-  total:5,
-  rating:0,
-  interactive:true,
-  clsStyl:{width:'25px', marginRight:'15px'}
-}
+  total: 5,
+  rating: 0,
+  interactive: true,
+  clsStyl: { width: '25px', marginRight: '15px' },
+};
