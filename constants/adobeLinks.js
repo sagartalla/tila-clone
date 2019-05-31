a48708c21f7796b11debd5401934ea1b99174108
sagar <sagar.talla@fptechscience.com>
2019-05-31 19:14:06 +0530

    Add Adobe tag

---
+++ constants/adobeLinks.js
@@ -0,0 +1,7 @@
+const links = {
+  'stage': '//assets.adobedtm.com/launch-ENc0358fe6617e4066a1c1c0ecff96f2e5-development.min.js',
+  'preprod': '//assets.adobedtm.com/launch-EN8715127157444dfb8bc778d15e16b3be.min.js',
+  'default': '//assets.adobedtm.com/launch-ENc0358fe6617e4066a1c1c0ecff96f2e5-development.min.js'
+}
+
+export default links;
