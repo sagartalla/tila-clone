a9fc6c6d1e3e4da7ee6773fdbfe15091cb60d14d
siddardha.raju <siddardha.raju@fptechscience.com>
2019-05-31 13:15:10 +0530

    Added toast for deleting the review

--- store/ratingReviews/api.js
+++ store/ratingReviews/api.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import constants from '../helper/constants';
 
 // const getReviewsRatings = (params) => axios.post(`${constants.REVIEWS_API_URL}/api/v1/reviews/`, {...params})
 
 const getMyReviews = () => axios.get(`${constants.REVIEWS_API_URL}/api/v1/reviews/my`);
 
-const deleteReview = id => axios.put(`${constants.REVIEWS_API_URL}/api/v1/reviews/delete/${id}`).then(() => id);
+const deleteReview = id => axios.put(`${constants.REVIEWS_API_URL}/api/v1/reviews/delete/${id}`).then(() => {
+  toast.success('Review Deleted Successfully!');
+  return id;
+});
 
 export default { getMyReviews, deleteReview };
