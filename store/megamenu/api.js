67094ef34bafa137c0f8f8c0762e83f0f4bcb93e
sagar <sagar.talla@fptechscience.com>
2019-06-13 19:33:37 +0530

    Fix

--- store/megamenu/api.js
+++ store/megamenu/api.js
@@ -5,6 +5,6 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 //TODO accept params
-const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/tree?countryCode=${cookies.get('country') || 'SAU'}&lang=${cookies.get('language')}&isListed=false`);
+const getMegamenu = () => axios.get(`${constants.CATEGORYTREE_API_URL}/mega-menu/tree?fl=itemColor&countryCode=${cookies.get('country') || 'SAU'}&lang=${cookies.get('language')}&isListed=false`);
 
 export default { getMegamenu };
