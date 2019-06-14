324b086e2978a86050f16171292431b162354a71
ashok chakravarthy <ashok@fptechscience.com>
2019-06-14 16:10:48 +0530

    Fixed CSS issues in My Order page issues

--- components/Order/includes/OrderDetails/OrderItem.js
+++ components/Order/includes/OrderDetails/OrderItem.js
@@ -127,7 +127,7 @@ class OrderItem extends Component {
     const showMsgAndDate = () => (
       <div className={`${styles['date-cont']} ${styles['flx-spacebw-alignc']}`}>
         <div>
-          <div className={styles['fs-12']}>{displayText()}</div>
+          <div className={`${styles['fs-14']} ${styles.fontW600}`}>{displayText()}</div>
           <div className={`${styles['ff-t']} ${styles['fs-24']} ${styles['ipad-fs-20']}`}>          
             {btnType === 'cancel' ? moment(orderItem.products[0].promisedDeliveryDate).format('ddd, MMM Do') : this.getDate(orderItem.products[0].state_time_estimates)}
           </div>
