dbfd69263b017270fd491895e9a485612b035594
Susmitha <susmitha@fptechscience.com>
2019-06-17 12:05:26 +0530

    few changes in flow

--- components/Login/VerifyStatus.js
+++ components/Login/VerifyStatus.js
@@ -3,7 +3,7 @@ import { Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { selectors, actionCreators } from '../../store/cam/personalDetails';
+import { selectors, actionCreators } from '../../store/auth';
 import { languageDefinations } from '../../utils/lang';
 import lang from '../../utils/language';
 import Button from '../common/CommonButton';
@@ -26,16 +26,9 @@ class VerifyStatus extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showInput: '',
+      otpValue: '',
     };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.forgotPasswordStatus === 'SUCCESS') {
-      this.setState({ showInput: true });
-    } else {
-      this.setState({ showInput: false });
-    }
+    this.sendLink = this.sendLink.bind(this);
   }
 
   sendLink(e) {
@@ -46,22 +39,11 @@ class VerifyStatus extends React.Component {
     const body = {
       email: activeEmailId,
     };
-    this.props.forgotPassword(body).then((res) => {
-      if (res && res.value && res.value.Response === 'SUCCESS') {
-        this.setState({ showInput: true });
-      } else {
-        this.setState({ showInput: false });
-      }
-    });
-    this.setState({
-      userNameError,
-      errorMsg,
-    });
-  }
-
-  resetShowLogin = () => {
-    const { resetShowLogin } = this.props;
-    resetShowLogin();
+    if (value === 'email') {
+      this.props.forgotPassword(body);
+    } else {
+      this.props.getMobileOtp(activeEmailId);
+    }
   }
 
   closeSuccessScreen = () => {
@@ -89,38 +71,38 @@ class VerifyStatus extends React.Component {
   }
 
   render() {
-    const { showOtpSuccess } = this.props;
-    const { showInput } =this.state;
+    const { showInput } = this.state;
+    const { showEmailSuccess, showOtpSuccess, loadingStatus, userData, activeEmailId } = this.props;
     return (
       <div className={`${styles['forgot-password']} ${styles.flex} ${styles['flex-colum']} ${styles['justify-around']}`}>
         <div>
           <h3 className={`${styles['fs-22']} ${styles['m-0']} ${styles['ff-b']}`}>{LOGIN_PAGE.FORGOT_PASSWORD}</h3>
         </div>
-      {true ?
-        <React.Fragment>
-          <div className={styles['reset-link']}>
-          <Row className={`${styles.flex}`}>
-            <Col md={8} className={`${styles['fs-14']}`}>{resetEmailLink}</Col>
-            <Col md={4} className={`${styles.flex}`}><SVGComponent clsName={`${styles['email-success-icon']}`} src="icons/common-icon/email-sent" /></Col>
-            </Row>
-            <a className={`${styles['text-blue']} ${styles['fs-12']} ${styles.pointer}`} onClick={this.sendLink}>{EMAIL_VERIFICATION.RESEND_THE_LINK}</a>   
-            <div className={`${styles.border} ${styles['m-10']}`} />
-            <div className={`${styles['fs-14']}`}>{EMAIL_VERIFICATION.PLEASE_CLICK_ON_EMAIL_LINK}</div>
-            <span className={styles['bg-light-gray']}>
-              <div className={`${styles['fs-12']}`}>
-                {EMAIL_VERIFICATION.IF_YOU_DO_NOT_RECEIVE_YOUR_EMAIL}
-                {EMAIL_VERIFICATION.CHECK_YOUR_SPAM_FOLDER}
-              </div>
-            </span>
-          </div>
-          <Button
-            className={`${styles['flex-center']}  ${styles.width100} ${styles['fs-14']} ${styles['text-uppercase']} ${styles['button-radius']}`}
-            btnText="OK"
-            onClick={this.resetLogin}
-          />
-        </React.Fragment>
+        {showEmailSuccess ?
+          <React.Fragment>
+            <div className={styles['reset-link']}>
+              <Row className={`${styles.flex}`}>
+                <Col md={7} className={`${styles['fs-14']}`}><span className={`${styles.flex} ${styles['flex-colum']}`}><span>{resetEmailLink}</span><span>{userData && userData.email}</span></span></Col>
+                <Col md={5} className={`${styles.flex}`}><SVGComponent clsName={`${styles['email-success-icon']}`} src="icons/common-icon/email-sent" /></Col>
+              </Row>
+              <a className={`${styles['text-blue']} ${styles['fs-12']} ${styles.pointer}`} data-id="email" onClick={this.sendLink}>{EMAIL_VERIFICATION.RESEND_THE_LINK}</a>
+              <div className={`${styles.border} ${styles['mt-10']} ${styles['mb-10']}`} />
+              <div className={`${styles['fs-14']}`}>{EMAIL_VERIFICATION.PLEASE_CLICK_ON_EMAIL_LINK}</div>
+              <span className={styles['bg-light-gray']}>
+                <div className={`${styles['fs-12']}`}>
+                  {EMAIL_VERIFICATION.IF_YOU_DO_NOT_RECEIVE_YOUR_EMAIL}
+                  &nbsp;{EMAIL_VERIFICATION.CHECK_YOUR_SPAM_FOLDER}
+                </div>
+              </span>
+            </div>
+            <Button
+              className={`${styles['flex-center']}  ${styles.width100} ${styles['fs-14']} ${styles['text-uppercase']} ${styles['button-radius']}`}
+              btnText="OK"
+              onClick={this.closeSuccessScreen}
+            />
+          </React.Fragment>
         :
-        showInput ?
+        showOtpSuccess &&
         <React.Fragment>
           <div className={styles['reset-link-mobile']}>
             <Row className={`${styles.flex}`}>
@@ -150,7 +132,7 @@ class VerifyStatus extends React.Component {
             onClick={this.sentOtpToReset}
             btnLoading={loadingStatus}
           />
-        </React.Fragment> : null
+        </React.Fragment>
       }
       </div>
     );
@@ -167,8 +149,11 @@ const mapStateToProps = store => ({
 });
 const mapDispatchToProps = dispatch => bindActionCreators(
   {
-    resetShowLogin: actionCreators.resetShowLogin,
-    forgotPassword: actionCreators.forgotPassword,    
+    forgotPassword: actionCreators.forgotPassword,
+    showNextPage: actionCreators.showNextPage,
+    verifyResetOtp: actionCreators.verifyResetOtp,
+    getMobileOtp: actionCreators.getMobileOtp,
+    v2CurrentFlow: actionCreators.v2CurrentFlow,
   },
   dispatch,
 );
