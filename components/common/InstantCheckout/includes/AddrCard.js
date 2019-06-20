7471857fd6628ae58c3e1ed4b8707e8901babb7c
siddardha.raju <siddardha.raju@fptechscience.com>
2019-06-20 16:31:28 +0530

    Issue 4 fixed

--- components/common/InstantCheckout/includes/AddrCard.js
+++ components/common/InstantCheckout/includes/AddrCard.js
@@ -10,13 +10,13 @@ import styles_ar from '../instant_ar.styl';
 
 const styles = lang === 'en' ? {...main_en, ...styles_en} : {...main_ar, ...styles_ar};
 
-const AddrCard = props => {
-  const { selectedAddr, toggleMiniAddress } = props;
+const AddrCard = (props) => {
+  const { selectedAddr, toggleMiniAddress, addressResults } = props;
   return (
-    <div className={`${styles['p-10-20']} ${styles['border-b']} ${styles['pointer']} ${styles['instant-check-list-inn']}`} onClick={toggleMiniAddress}>
-      <h5 className={`${styles['mt-5']} ${styles['fontW600']} ${styles['mb-5']} ${styles['thick-gry-clr']}`}>{selectedAddr.first_name} {selectedAddr.last_name}</h5>
+    <div className={`${styles['p-10-20']} ${styles['border-b']} ${styles.pointer} ${styles.relative} ${addressResults.length > 1 && styles['instant-check-list-inn']}`} onClick={toggleMiniAddress}>
+      <h5 className={`${styles['mt-5']} ${styles.fontW600} ${styles['mb-5']} ${styles['thick-gry-clr']}`}>{selectedAddr.first_name} {selectedAddr.last_name}</h5>
       <span className={`${styles['fs-12']} ${styles['label-gry-clr']}`}>
-          {selectedAddr.address_line_1 + ', ' + selectedAddr.address_line_2 + ', ' + selectedAddr.city + ', ' + selectedAddr.country_name + ', ' + selectedAddr.postal_code}
+        {selectedAddr.address_line_1 + ', ' + selectedAddr.address_line_2 + ', ' + selectedAddr.city + ', ' + selectedAddr.country_name + ', ' + selectedAddr.postal_code}
       </span>
     </div>
 
