5e90cf5bc7a59fe5445ea261d874691f740cc98f
sagar <sagar.talla@fptechscience.com>
2019-07-02 11:42:25 +0530

    Revert "Sticky sidebar"
    This reverts commit fe1b45ec583cf8c3ada52630163c47df9ded95b1.

--- components/Product/index.js
+++ components/Product/index.js
@@ -98,16 +98,16 @@ const getProductComponent = (isPreview, taskCode) => {
         }
       }
 
-      // window.addEventListener('scroll', this.handleScroll);
-      // setTimeout(() => {
-      //   const shippingContainer = document.getElementById('shipping-cont');
-      //   const buttonsCont = document.getElementById('cart-btn-cont');
-      //   const [{height: shippingHeight, top: shippingY}, {height: btnHeight}] = [shippingContainer.getBoundingClientRect(), buttonsCont.getBoundingClientRect()];
-      //   skipScroll = (shippingHeight + shippingY) < (window.innerHeight - btnHeight);
-      //   this.setState({
-      //     defaultPosition: skipScroll ? 'absolute-style' : 'fixed-style'
-      //   });
-      // });
+      window.addEventListener('scroll', this.handleScroll);
+      setTimeout(() => {
+        const shippingContainer = document.getElementById('shipping-cont');
+        const buttonsCont = document.getElementById('cart-btn-cont');
+        const [{height: shippingHeight, top: shippingY}, {height: btnHeight}] = [shippingContainer.getBoundingClientRect(), buttonsCont.getBoundingClientRect()];
+        skipScroll = (shippingHeight + shippingY) < (window.innerHeight - btnHeight);
+        this.setState({
+          defaultPosition: skipScroll ? 'absolute-style' : 'fixed-style'
+        });
+      });
     }
 
     componentWillUnmount() {
@@ -210,72 +210,9 @@ const getProductComponent = (isPreview, taskCode) => {
               isPreview || isSearchPreview ? null : <HeaderBar />
             }
             <div className={`${styles.relative}`}>
-              <Col md={4} sm={12} className={`${styles['details-right-part']} ${styles[stickyElements.details]} ${styles['p-0']}`}>
-                <div className={`${styles['details-right-part-inn']}`}>
-                  <div className={`${styles['ipad-details']} ${styles['ipad-pr-15']}`}>
-                    <TitleInfo {...titleInfo} isPreview={isPreview} offerInfo={offerInfo} />
-                    <ProductDetails
-                      details={details}
-                      keyfeatures={keyfeatures}
-                      isPreview={isPreview}
-                      productInfo={productData}
-                      variantId={variantId}
-                      productId={productId}
-                      isSearchPreview={isSearchPreview}
-                    />
-                  </div>
-                  <div className={`${styles['ipad-details']} ${styles['bdr-lt']} ${styles['ipad-pl-15']}`}>
-                    {
-                      isPreview ? null : <Shipping shippingInfo={shippingInfo} returnInfo={returnInfo} offerInfo={offerInfo} warranty={warranty} />
-                    }
-                    {isPreview ? null :
-                      (shippingInfo === null || shippingInfo.shippable)
-                        ?
-                        <AddToCart
-                          offerInfo={offerInfo}
-                          productData={productData.product_id}
-                          shippingInfo={shippingInfo}
-                          isPreview={isPreview}
-                          emailErr={emailErr}
-                          userDetails={userDetails}
-                          notifyEmail={notifyEmail}
-                          notify={this.notify}
-                          showLoading={showLoading}
-                          onChangeField={this.onChangeField}
-                          styling={isSearchPreview ? null : positionStyle || defaultPosition}
-                          top={isSearchPreview ? null : positionStyle === 'absolute-style' ?  positionTop : null}
-                        />
-                        :
-                        null
-                    }
-
-                    {/* {isPreview ? null :
-                      (offerInfo.stockError || offerInfo.availabilityError) && ((shippingInfo && Object.keys(shippingInfo).length === 0) || (shippingInfo === null || shippingInfo.shippable)) &&
-                      <div className={`${styles['flx-space-bw']} ${styles['align-baseline']}`}>
-                        {!userDetails.isLoggedIn &&
-                        <div className={`${styles['mb-0']} ${styles['fp-input']} ${styles['pb-10']}`}>
-                          <input onChange={this.onChangeField} name="notify" type="text" value={notifyEmail} required />
-                          <label>{PDP_PAGE.GET_NOTIFIED}</label>
-                          {emailErr &&
-                            <span className={styles['error-msg']}>{emailErr}</span>
-                          }
-                        </div>}
-                        <Button
-                          className={`${styles['flex-center']} ${styles.notify_me_btn} ${styles['fs-20']}`}
-                          btnText={PDP_PAGE.NOTIFY_ME}
-                          onClick={this.notify}
-                          hoverClassName="hoverBlueBackground"
-                          btnLoading={showLoading}
-                        />
-                      </div>
-                    } */}
-                  </div>
-                </div>
-
-              </Col>
               <div className={`${styles['page-details-slider']}`}>
                 <Row className={`${styles['m-0']} ${styles['ht-100per']}`}>
-                  <Col xs={12} md={8} sm={12} className={`${styles['pl-0']} ${styles['pdp-img-prt']} ${styles['p-0']}`}>
+                  <Col xs={12} md={8} sm={12} className={`${styles['pl-0']} ${styles['pdp-img-prt']}`}>
                     <NoSSR>
                       <Display
                         product_id={product_id}
@@ -290,6 +227,69 @@ const getProductComponent = (isPreview, taskCode) => {
                     </NoSSR>
                   </Col>
                   <div className={styles['details-pixel']} ref={this.detailsRef} />
+                  <Col sm={12} className={`${styles['details-right-part']} ${styles[stickyElements.details]}`}>
+                    <div className={`${styles['details-right-part-inn']}`}>
+                      <div className={`${styles['ipad-details']} ${styles['ipad-pr-15']}`}>
+                        <TitleInfo {...titleInfo} isPreview={isPreview} offerInfo={offerInfo} />
+                        <ProductDetails
+                          details={details}
+                          keyfeatures={keyfeatures}
+                          isPreview={isPreview}
+                          productInfo={productData}
+                          variantId={variantId}
+                          productId={productId}
+                          isSearchPreview={isSearchPreview}
+                        />
+                      </div>
+                      <div className={`${styles['ipad-details']} ${styles['bdr-lt']} ${styles['ipad-pl-15']}`}>
+                        {
+                          isPreview ? null : <Shipping shippingInfo={shippingInfo} returnInfo={returnInfo} offerInfo={offerInfo} warranty={warranty} />
+                        }
+                        {isPreview ? null :
+                          (shippingInfo === null || shippingInfo.shippable)
+                            ?
+                            <AddToCart
+                              offerInfo={offerInfo}
+                              productData={productData.product_id}
+                              shippingInfo={shippingInfo}
+                              isPreview={isPreview}
+                              emailErr={emailErr}
+                              userDetails={userDetails}
+                              notifyEmail={notifyEmail}
+                              notify={this.notify}
+                              showLoading={showLoading}
+                              onChangeField={this.onChangeField}
+                              styling={isSearchPreview ? null : positionStyle || defaultPosition}
+                              top={isSearchPreview ? null : positionStyle === 'absolute-style' ?  positionTop : null}
+                            />
+                            :
+                            null
+                        }
+
+                        {/* {isPreview ? null :
+                          (offerInfo.stockError || offerInfo.availabilityError) && ((shippingInfo && Object.keys(shippingInfo).length === 0) || (shippingInfo === null || shippingInfo.shippable)) &&
+                          <div className={`${styles['flx-space-bw']} ${styles['align-baseline']}`}>
+                            {!userDetails.isLoggedIn &&
+                            <div className={`${styles['mb-0']} ${styles['fp-input']} ${styles['pb-10']}`}>
+                              <input onChange={this.onChangeField} name="notify" type="text" value={notifyEmail} required />
+                              <label>{PDP_PAGE.GET_NOTIFIED}</label>
+                              {emailErr &&
+                                <span className={styles['error-msg']}>{emailErr}</span>
+                              }
+                            </div>}
+                            <Button
+                              className={`${styles['flex-center']} ${styles.notify_me_btn} ${styles['fs-20']}`}
+                              btnText={PDP_PAGE.NOTIFY_ME}
+                              onClick={this.notify}
+                              hoverClassName="hoverBlueBackground"
+                              btnLoading={showLoading}
+                            />
+                          </div>
+                        } */}
+                      </div>
+                    </div>
+
+                  </Col>
                 </Row>
               </div>
               {
