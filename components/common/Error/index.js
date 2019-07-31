d37bbef955ccce4247399b30bdd5c186fe6a91d3
ashok chakravarthy <ashok@fptechscience.com>
2019-07-31 14:13:09 +0530

    Modifie text

--- components/common/Error/index.js
+++ components/common/Error/index.js
@@ -9,17 +9,17 @@ import styles_ar from './error_ar.styl';
 const styles = lang === 'en' ? {...main_en, ...styles_en} : {...main_ar, ...styles_ar};
 
 const getMessage = (statusCode) => {
-  return 'SOMETHING WENT WRONG';
+  return 'Something went wrong';
 }
 const Error = ({ statusCode, messege }) => {
-  messege = messege || 'SOMETHING WENT WRONG';
+  messege = messege || 'Something went wrong';
   return (
     <div className={`${styles['errors-main']}`}>
       <div className={`${styles['errors-main-inn']}`}>
         <div className={`${styles['erros-inn-img']}`}>
           <img className={styles['']} src={"/static/img/errors-img/404tila.png"} />
         </div>
-        <span className={styles['label-not']}>SOMETHING WENT WRONG</span>
+        <span className={styles['label-not']}>Something went wrong</span>
       </div>
     </div>
   );
