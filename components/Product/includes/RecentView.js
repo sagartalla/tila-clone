6ed757c882df1551acf56d1f25ca18b13b2c1ee5
siddardha.raju <siddardha.raju@fptechscience.com>
2019-08-12 12:55:17 +0530

    changes

--- components/Product/includes/RecentView.js
+++ components/Product/includes/RecentView.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import { Col } from 'react-bootstrap';
 import Slider from 'react-slick';
-// import constants from '../../../constants';
 import constants from '../../../constants';
 
 import { languageDefinations } from '../../../utils/lang';
@@ -14,7 +12,7 @@ import main_ar from '../../../layout/main/main_ar.styl';
 import styles_en from '../product_en.styl';
 import styles_ar from '../product_ar.styl';
 
-const styles = lang === 'en' ? {...main_en, ...styles_en} : {...main_ar, ...styles_ar};
+const styles = lang === 'en' ? { ...main_en, ...styles_en } : { ...main_ar, ...styles_ar };
 
 const { PDP_PAGE } = languageDefinations();
 
@@ -30,18 +28,18 @@ const RecentView = (props) => {
   if (recentlyViewed.length === 0) return null;
   return (
     <div className={`${styles['recentview-main']} ${styles['pb-25']}`}>
-      <h6 className={`${styles['recent-title']} ${styles['pt-15']} ${styles['pb-15']} ${styles['pl-15']} ${styles['fontW600']}`}><span className={styles['pl-15']}>{PDP_PAGE.RECENTLY_VIEWED}</span></h6>
+      <h6 className={`${styles['recent-title']} ${styles['pt-15']} ${styles['pb-15']} ${styles['pl-15']} ${styles.fontW600}`}><span className={styles['pl-15']}>{PDP_PAGE.RECENTLY_VIEWED}</span></h6>
       <div>
         <Slider {...settings}>
           {
             recentlyViewed.map(item => (
               <Col md={2} className={`${styles['pl-0']}`} key={item.id}>
-                <a href={`/${lang}/pdp/${item.nm.replace(/\//g, '').split(' ').join('-').toLowerCase()}/${item.tuinId ? `${item.tuinId}/`: '' }${item.id ? `${item.id}/`: ''}?pid=${item.pid}&vid=${item.vid}&cid=${item.cid}`}>
-                  <div className={`${styles['recentview-main-inn']} ${styles['flex']} ${styles['flex-colum']}`}>
+                <a href={`/${lang}/pdp/${item.nm.replace(/\//g, '').split(' ').join('-').toLowerCase()}/${item.tuinId ? `${item.tuinId}/` : ''}${item.id ? `${item.id}/` : ''}?pid=${item.pid}&vid=${item.vid}&cid=${item.cid}`}>
+                  <div className={`${styles['recentview-main-inn']} ${styles.flex} ${styles['flex-colum']}`}>
                     <div className={styles['recentview-main-inn-img']}><img src={`${constants.mediaDomain}/${item.im}`} className="img-responsive" /></div>
                     <div>
-                      <h6 className={`${styles['fs-10']} ${styles['mb-0']} ${styles['thick-gry-clr']} ${styles['elipsis']}`}>{item.nm}</h6>
-                      <span className={`${styles['fs-12']} ${styles['thick-gry-clr']}`}><span className={styles['fontW600']}>{item.pr} </span><span>{item.cd}</span></span>
+                      <h6 className={`${styles['fs-10']} ${styles['mb-0']} ${styles['thick-gry-clr']} ${styles.elipsis}`}>{item.nm}</h6>
+                      <span className={`${styles['fs-12']} ${styles['thick-gry-clr']}`}><span className={styles.fontW600}>{item.pr} </span><span>{item.cd}</span></span>
                     </div>
                   </div>
                 </a>
@@ -52,6 +50,6 @@ const RecentView = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default RecentView;
