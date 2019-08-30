da7822e9dbe3d04e4a0c6033acc4b8d74d24e213
Susmitha <susmitha@fptechscience.com>
2019-08-30 12:18:19 +0530

    CAM-1364

--- components/Cam/Orders/index.js
+++ components/Cam/Orders/index.js
@@ -81,7 +81,7 @@ class Orders extends Component {
               <div className={`${styles['flex']} ${styles['no-order-part-inn']}`}>
                 <SVGComponent clsName={`${styles['no-order-list-icon']}`} src="icons/common-icon/no-order-icon" />
                 <h4 className={`${styles['fs-24']} ${styles['t-c']} ${styles['pt-40']}`}>{ORDERS.NO_WISHLIST_LABEL}</h4>
-                <div className={`${styles['fs-14']} ${styles['thick-gry-clr']}`}>{ORDERS.START_SHOPPING_TO_MAKE_KITTEN_HAPPY}</div>
+                <div className={`${styles['fs-14']} ${styles['thick-gry-clr']} ${styles['pt-10']}`}>{ORDERS.START_SHOPPING_TO_MAKE_KITTEN_HAPPY}</div>
                 <a href={`/${language}`} className={`${styles['fp-btn']} ${styles['fp-btn-primary']} ${styles['fp-btn-x-large']} ${styles['right-radius']} ${styles['text-uppercase']} ${styles['fontW600']} ${styles['mt-40']}`}>{ORDERS.START_SHOPPING}</a>
               </div>
             </div>
