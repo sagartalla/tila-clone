b87ff85c53a37a8596daa4e9f3631beca9e57874
Mayur Ray <shiremen@gmail.com>
2019-09-21 17:27:58 +0530

    image_outside_click

--- components/common/PopUp/index.js
+++ components/common/PopUp/index.js
@@ -5,21 +5,11 @@ const PopUp = ({
   isOpen, width, height, closePopUp, children,
 }) => (
   isOpen &&
-  <div className={styles["popup"]}>
+  <div className={styles["popup"]} onClick={closePopUp}>
     <div
       className={styles["inner"]}
       style={{ width, height }}
     >
-      <div style={{ display: 'flex', margin: '15px' }}>
-        <span
-          role="button"
-          onClick={closePopUp}
-          tabIndex="0"
-          className={styles["cross"]}
-        >
-          &times;
-        </span>
-      </div>
       {children}
     </div>
   </div>
