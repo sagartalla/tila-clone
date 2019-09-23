b2748045d115c0fd442c2810253d3bae8975edf9
sagar talla <sagar.talla@fptechscience.com>
2019-09-23 10:39:10 +0530

    Remove deactivation

--- constants/adobeLinks.js
+++ constants/adobeLinks.js
@@ -1,6 +1,6 @@
 const links = {
   'stage': '//assets.adobedtm.com/launch-ENf8a881fcc7674a3c8d185fc177def2c9-staging.min.js',
-  'preprod': '//assets.adobedtm.com/launch-EN8715127157444dfb8bc778d15e16b3be.min.js',
+  'preprod': '//assets.adobedtm.com/launch-ENf8a881fcc7674a3c8d185fc177def2c9-staging.min.js',
   'prod': '//assets.adobedtm.com/launch-EN8715127157444dfb8bc778d15e16b3be.min.js',
   'default': '//assets.adobedtm.com/launch-ENc0358fe6617e4066a1c1c0ecff96f2e5-development.min.js'
 }
